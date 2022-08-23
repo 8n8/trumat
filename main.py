52efbdbb67fb9433f32567418f45ede0ad608a55
True Ghiassi <true@ghiassitrio.co.uk>
2022-08-23 21:20:39 +0100

    The linter now works with different values for the quoted string in the minimal example.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+def up_to_double_quote(src, pos):
+    i = 0
+    for i, char in enumerate(src[pos:]):
+        if char == '"':
+            break
+
+    return src[pos : i + pos], i + pos
+
+
 def lint(unlinted):
+    _, i = up_to_double_quote(unlinted["src/Main.elm"], 0)
+    quoted, _ = up_to_double_quote(unlinted["src/Main.elm"], i + 1)
+    print("quoted:", quoted)
     return {
-        "src/Main.elm": """module Main exposing (main)
+        "src/Main.elm": f"""module Main exposing (main)
 
 import Html
 
 main =
-    Html.text "hi"
+    Html.text "{quoted}"
 """
     }
