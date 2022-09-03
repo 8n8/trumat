68e97a665e291c9a0850adb2bbca04809094a998
True Ghiassi <true@ghiassitrio.co.uk>
2022-09-03 21:39:45 +0100

    Fixed a load of errors in the token parser.

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 import json
-import sys
+import os
+import array
 
 MODULE_SEPARATOR = 255
 
-# Types - these must be different
-ORDINARY_STRING_LITERAL_TYPE = 0
-TRIPLE_QUOTED_STRING_LITERAL_TYPE = 1
-FLOAT_TYPE = 2
-INT_TYPE = 3
+
+class TOKEN:
+    ORDINARY_STRING_LITERAL = 0
+    TRIPLE_QUOTED_STRING_LITERAL = 1
+    FLOAT = 2
+    INT = 3
+    MODULE = 4
+    EXPOSING = 5
+    EQUALS = 6
+    COLON = 7
+    ARROW = 8
+    IF = 9
+    ELSE = 10
+    AS = 11
+    CASE = 12
+    OF = 13
+    COMMA = 14
+    OPEN_SQUARE_BRACKET = 15
+    CLOSE_SQUARE_BRACKET = 16
+    OPEN_PARENTHESIS = 17
+    CLOSE_PARENTHESIS = 18
+    LESS_OR_EQUAL = 19
+    MORE_OR_EQUAL = 20
+    LESS_THAN = 21
+    MORE_THAN = 22
+    NOT_EQUAL = 23
+    EQUAL = 24
+    MODULUS = 25
+    AND = 26
+    OR = 27
+    NOT = 28
+    MULTIPLY = 29
+    PLUS = 30
+    MINUS = 31
+    DIVIDE_INT = 32
+    DIVIDE_FLOAT = 33
+    IMPORT = 34
+    MODULE_SEPARATOR = 35
 
 
 def get_elm_paths(raw):
@@ -20,6 +54,20 @@ def get_elm_paths(raw):
     return paths
 
 
+def parse_module_separator(tokens, src, i):
+    try:
+        if src[i] != MODULE_SEPARATOR:
+            return i
+    except IndexError:
+        return i
+
+    tokens["type"].append(TOKEN.MODULE_SEPARATOR)
+    tokens["start"].append(i)
+    i += 1
+    tokens["end"].append(i)
+    return i
+
+
 def parse_ordinary_string(tokens, src, i):
     try:
         if src[i] != ord('"'):
@@ -28,83 +76,57 @@ def parse_ordinary_string(tokens, src, i):
         return i
 
     i += 1
-    tokens['type'].append(ORDINARY_STRING_LITERAL_TYPE)
-    tokens['start'].append(i)
+    tokens["type"].append(TOKEN.ORDINARY_STRING_LITERAL)
+    tokens["start"].append(i)
 
-    try:
-        while True:
-            if src[i] == ord('\\'):
-                i += 2
-                continue
+    while True:
+        if src[i] == ord("\\"):
+            i += 2
+            continue
 
-            if src[i] == ord('"'):
-                tokens['end'].append(i)
-                return i+1
+        if src[i] == ord('"'):
+            tokens["end"].append(i)
+            return i + 1
 
-            i += 1
-    except IndexError:
-        raise InvalidElm("I was parsing an ordinary string, but got the end of the input without finding the closing quote mark.")
+        i += 1
 
 
-def parse_triple_quoted_string(src, i, module_id, ast):
+def parse_triple_quoted_string(tokens, src, i):
     # try to parse the opening """
-    if parse_token(src, i, b'"""') != i + 3:
+    if parse_exact(src, i, b'"""') != i + 3:
         # this isn't a triple-quoted string so stop and backtrack
         return i
 
     i += 3
-    tokens['type'].append(TRIPLE_QUOTED_STRING_LITERAL_TYPE)
-    tokens['start'].append(i)
+    tokens["type"].append(TOKEN.TRIPLE_QUOTED_STRING_LITERAL)
+    tokens["start"].append(i)
 
-    try:
-        while True:
-            if src[i] == ord('\\'):
-                i += 2
-                continue
+    while True:
+        if src[i] == ord("\\"):
+            i += 2
+            continue
 
-            if parse_token(src, i, b'"""') == i + 3:
-                tokens['end'].append(i)
-                return i+3
+        if parse_exact(src, i, b'"""') == i + 3:
+            tokens["end"].append(i)
+            return i + 3
 
-            i += 1
-
-    except IndexError:
-        raise InvalidElm("I was parsing a triple-quoted string, but got the end of the input without finding the closing quote marks.")
+        i += 1
 
 
 def is_digit(ch):
-    return (ch >= ord('0') and ch <= ord('9')) or ch == ord('.')
+    return (ch >= ord("0") and ch <= ord("9")) or ch == ord(".")
 
 
 def is_float_char(ch):
-    return (ch >= ord('0') and ch <= ord('9')) or ch == ord('.')
+    return (ch >= ord("0") and ch <= ord("9")) or ch == ord(".")
 
 
-def parse_float(tokens, src, i):
-    try:
-        if not is_float_char(src[i]):
-            return i
-    except IndexError:
-        return i
-    
-    tokens['type'].append(FLOAT_TYPE)
-    tokens['start'].append(i)
-
-    i += 1
-
-    try:
-        while is_float_char(src[i]):
-            i += 1
-
-    except IndexError:
-        pass
-
-    tokens['end'].append(i)
-    return i
-
-
-def is_valid_hex_char(ch):
-    return (ch >= ord('a') and ch <= ord('f')) or (ch >= ord('A') and ch <= ord('F')) or is_digit(ch)
+def is_hex_char(ch):
+    return (
+        (ch >= ord("a") and ch <= ord("f"))
+        or (ch >= ord("A") and ch <= ord("F"))
+        or is_digit(ch)
+    )
 
 
 def parse_number(tokens, raw, i):
@@ -112,7 +134,7 @@ def parse_number(tokens, raw, i):
     if j > i:
         return j
 
-    if raw[i] != ord('-'):
+    if raw[i] != ord("-"):
         return i
 
     i += 1
@@ -123,115 +145,114 @@ def parse_positive_number(tokens, raw, i):
     if not is_digit(raw[i]):
         return i
 
-    tokens['start'].append(i) 
+    tokens["start"].append(i)
     i += 1
 
     is_float = False
     while i < len(raw):
-        if is_digit(raw[i]) or is_hex_letter(raw[i]) or raw[i] == ord('x'):
+        if is_digit(raw[i]) or is_hex_char(raw[i]) or raw[i] == ord("x"):
             i += 1
             continue
 
-        if raw[i] == ord('.'):
+        if raw[i] == ord("."):
             is_float = True
             i += 1
             continue
 
-        if raw[i] == ord('e'):
+        if raw[i] == ord("e"):
             is_float = True
             i += 1
             continue
 
         break
 
-    tokens['end'].append(i)
+    tokens["end"].append(i)
     if is_float:
-        tokens['type'].append(FLOAT_TYPE)
+        tokens["type"].append(TOKEN.FLOAT)
         return i
 
-    tokens['type'].append(INT_TYPE)
+    tokens["type"].append(TOKEN.INT)
     return i
 
 
 def is_subsequent_name_char(ch):
     return (
-        (ch >= ord('a') and ch <= ord('z'))
-        or (ch >= ord('A') and ch <= ord('Z'))
-        or ch == ord('_'))
+        (ch >= ord("a") and ch <= ord("z"))
+        or (ch >= ord("A") and ch <= ord("Z"))
+        or ch == ord("_")
+    )
 
 
-def parse_capitalised_name(tokens, raw, i):
+def parse_capitalised_name(tokens, src, i):
     try:
-        if not (src[i] >= ord('A') and src[i] <= ord('Z')):
+        if not (src[i] >= ord("A") and src[i] <= ord("Z")):
             return i
     except IndexError:
         return i
-    tokens['start'].append(i)
+    tokens["start"].append(i)
     i += 1
 
-    while i < len(raw):
-        if not is_subsequent_name_char(raw[i]):
+    while i < len(src):
+        if not is_subsequent_name_char(src[i]):
             break
         i += 1
 
-    tokens['end'].append(i)
-    tokens['type'].append(CAPITALISED_NAME_TYPE)
+    tokens["end"].append(i)
+    tokens["type"].append(TOKEN.CAPITALISED_NAME)
     return i
 
 
-def parse_variable_name(tokens, raw, i):
+def parse_variable_name(tokens, src, i):
     try:
-        if not (
-            (src[i] >= ord('a') and src[i] <= ord('z')
-            or src[i] == ord('_')):
+        if not ((src[i] >= ord("a") and src[i] <= ord("z")) or src[i] == ord("_")):
             return i
     except IndexError:
         return i
-    tokens['start'].append(i)
+    tokens["start"].append(i)
     i += 1
 
-    while i < len(raw):
-        if not is_subsequent_name_char(raw[i]):
+    while i < len(src):
+        if not is_subsequent_name_char(src[i]):
             break
         i += 1
 
-    tokens['end'].append(i)
-    tokens['type'].append(VARIABLE_NAME_TYPE)
+    tokens["end"].append(i)
+    tokens["type"].append(TOKEN.VARIABLE_NAME)
     return i
 
 
 keywords = [
-    ("module", MODULE),
-    ("import", IMPORT),
-    ("exposing", EXPOSING),
-    ("=", EQUALS),
-    (":", COLON),
-    ("->", ARROW),
-    ("if", IF),
-    ("else", ELSE),
-    ("as", AS),
-    ("case", CASE),
-    ("of", OF),
-    (",", COMMA),
-    ("[", OPEN_SQUARE_BRACKET),
-    ("]", CLOSE_SQUARE_BRACKET),
-    ("(", OPEN_PARENTHESIS),
-    (")", CLOSE_PARENTHESIS),
-    ("<=", LESS_OR_EQUAL),
-    (">=", MORE_OR_EQUAL),
-    ("<", LESS_THAN),
-    (">", MORE_THAN),
-    ("/=", NOT_EQUAL),
-    ("==", EQUAL),
-    ("%", MODULUS),
-    ("&&", AND),
-    ("||", OR),
-    ("not", NOT),
-    ("*", MULTIPLY),
-    ("+", PLUS),
-    ("-", MINUS),
-    ("//", DIVIDE_INT),
-    ("/", DIVIDE_FLOAT)
+    ("module", TOKEN.MODULE),
+    ("import", TOKEN.IMPORT),
+    ("exposing", TOKEN.EXPOSING),
+    ("=", TOKEN.EQUALS),
+    (":", TOKEN.COLON),
+    ("->", TOKEN.ARROW),
+    ("if", TOKEN.IF),
+    ("else", TOKEN.ELSE),
+    ("as", TOKEN.AS),
+    ("case", TOKEN.CASE),
+    ("of", TOKEN.OF),
+    (",", TOKEN.COMMA),
+    ("[", TOKEN.OPEN_SQUARE_BRACKET),
+    ("]", TOKEN.CLOSE_SQUARE_BRACKET),
+    ("(", TOKEN.OPEN_PARENTHESIS),
+    (")", TOKEN.CLOSE_PARENTHESIS),
+    ("<=", TOKEN.LESS_OR_EQUAL),
+    (">=", TOKEN.MORE_OR_EQUAL),
+    ("<", TOKEN.LESS_THAN),
+    (">", TOKEN.MORE_THAN),
+    ("/=", TOKEN.NOT_EQUAL),
+    ("==", TOKEN.EQUAL),
+    ("%", TOKEN.MODULUS),
+    ("&&", TOKEN.AND),
+    ("||", TOKEN.OR),
+    ("not", TOKEN.NOT),
+    ("*", TOKEN.MULTIPLY),
+    ("+", TOKEN.PLUS),
+    ("-", TOKEN.MINUS),
+    ("//", TOKEN.DIVIDE_INT),
+    ("/", TOKEN.DIVIDE_FLOAT),
 ]
 
 
@@ -239,9 +260,9 @@ def parse_keyword(tokens, raw, i):
     for keyword, type_ in keywords:
         j = parse_exact(raw, i, keyword)
         if j > i:
-            tokens['start'].append(i)
-            tokens['type'].append(type_)
-            tokens['end'].append(j)
+            tokens["start"].append(i)
+            tokens["type"].append(type_)
+            tokens["end"].append(j)
             return j
 
         i = j
@@ -249,22 +270,22 @@ def parse_keyword(tokens, raw, i):
     return i
 
 
-def parse_line_comment(tokens, raw, i):
-    j = parse_exact(raw, i, "--")
+def parse_line_comment(tokens, src, i):
+    j = parse_exact(src, i, "--")
     if j == i:
         return i
     i = j
 
-    tokens['type'].append(LINE_COMMENT)
-    tokens['start'].append(i)
+    tokens["type"].append(TOKEN.LINE_COMMENT)
+    tokens["start"].append(i)
 
-    while i < len(raw):
-        if src[i] == ord('\n'):
+    while i < len(src):
+        if src[i] == ord("\n"):
             break
 
         i += 1
 
-    tokens['end'].append(i)
+    tokens["end"].append(i)
     return i
 
 
@@ -281,27 +302,27 @@ def parse_block_comment_help(tokens, raw, i, start):
     if j == i:
         return i
     i = j
-    
-    tokens['type'].append(BLOCK_COMMENT)
-    tokens['start'].append(i)
+
+    tokens["type"].append(TOKEN.BLOCK_COMMENT)
+    tokens["start"].append(i)
 
     while True:
         if parse_exact(raw, i, "-}") == i:
             i += 1
             continue
 
-        break 
+        break
 
-    tokens['end'].append(i)
-    return i+2
+    tokens["end"].append(i)
+    return i + 2
 
 
 def parse_spaces(tokens, src, i):
-    return parse_spaces_help(tokens, src, i, ' ', SPACES)
+    return parse_spaces_help(tokens, src, i, " ", TOKEN.SPACES)
 
 
 def parse_newlines(tokens, src, i):
-    return parse_spaces_help(tokens, src, i, '\n', NEWLINES)
+    return parse_spaces_help(tokens, src, i, "\n", TOKEN.NEWLINES)
 
 
 def parse_spaces_help(tokens, raw, i, char, type_):
@@ -311,10 +332,10 @@ def parse_spaces_help(tokens, raw, i, char, type_):
     except IndexError:
         return i
 
-    tokens['start'].append(i)
+    tokens["start"].append(i)
     i += 1
 
-    tokens['type'].append(type_)
+    tokens["type"].append(type_)
     while True:
         try:
             if raw[i] != char:
@@ -324,19 +345,8 @@ def parse_spaces_help(tokens, raw, i, char, type_):
 
         i += 1
 
-    tokens['end'].append(i) 
+    tokens["end"].append(i)
     return i
-
-
-token_parsers = [
-    parse_whitespace,
-    parse_keyword,
-    parse_capitalised_name,
-    parse_variable_name,
-    parse_number,
-    parse_triple_quoted_string,
-    parse_ordinary_string
-]
 
 
 def parse_many(tokens, raw, i, parsers):
@@ -352,9 +362,10 @@ def parse_many(tokens, raw, i, parsers):
 
 def tokenize(raw):
     tokens = {
-        'type': array.array('B'),
-        'start': array.array('L'),
-        'end': array.array('L')}
+        "type": array.array("B"),
+        "start": array.array("L"),
+        "end": array.array("L"),
+    }
 
     i = 0
     while i < len(raw):
@@ -362,37 +373,42 @@ def tokenize(raw):
             tokens,
             raw,
             i,
-            [parse_spaces,
-             parse_newlines,
-             parse_line_comment,
-             parse_block_comment,
-             parse_keyword,
-             parse_capitalised_name,
-             parse_variable_name,
-             parse_number,
-             parse_triple_quoted_string,
-             parse_ordinary_string])
+            [
+                parse_spaces,
+                parse_newlines,
+                parse_line_comment,
+                parse_block_comment,
+                parse_keyword,
+                parse_capitalised_name,
+                parse_variable_name,
+                parse_number,
+                parse_triple_quoted_string,
+                parse_ordinary_string,
+                parse_module_separator,
+            ],
+        )
 
     return tokens
 
 
 def read_sources(source_directories):
-    src = b''
-    for source_directory in elm_dot_json['source-directories']:
+    src = b""
+    for source_directory in source_directories:
         for path in get_elm_paths(os.walk(source_directory)):
-            with open(path, 'rb') as elm:
-                src += bytes([MODULE_SEPARATOR]) + f.read()
+            with open(path, "rb") as elm:
+                src += bytes([MODULE_SEPARATOR]) + elm.read()
 
     return src
 
 
 def main():
-    with open('elm.json', 'rb') as f:
+    with open("elm.json", "rb") as f:
         elm_dot_json = json.load(f)
 
-    src = read_sources(elm_dot-json['source-directories'])
+    src = read_sources(elm_dot_json["source-directories"])
 
     ast = parse(tokenize(src))
+
 
 main()
 
@@ -402,7 +418,7 @@ def parse_exact(src, i, token):
 
     try:
         while j < len(token):
-            if src[i+j] != token[j]:
+            if src[i + j] != token[j]:
                 # a source character doesn't match a token character
                 return i
 
