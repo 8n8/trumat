7c2b4dba9221dd89d7d708db73917384c3e4317d
True Ghiassi <true@ghiassitrio.co.uk>
2022-10-04 22:18:33 +0100

    Mainly formatting

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define MAX_PATH 4096
 
 void make_file_path(
-    char file_path[MAX_PATH],
+	char file_path[MAX_PATH],
 	char directory_path[MAX_PATH],
 	char file_name[256]) {
 
@@ -25,13 +25,13 @@ void make_file_path(
 	// copy the file name
 	for (int j = 0; j < 256; ++j) {
 		if (file_name[j] == '\0') {
-            file_path[i+j] = '\0';
+			file_path[i+j] = '\0';
 			return;
 		}
 
 		file_path[i + j] = file_name[j];
 	}
-    printf("SHOULDNT HAPPEN");
+	printf("SHOULDNT HAPPEN");
 }
 
 void make_sub_dir_path(
@@ -53,7 +53,7 @@ void make_sub_dir_path(
 
 	for (int j = 0; j < 256; ++j) {
 		if (sub_dir_name[j] == '\0') {
-            sub_dir_path[i+j] = '\0';
+			sub_dir_path[i+j] = '\0';
 			return;
 		}
 		sub_dir_path[i + j] = sub_dir_name[j];
@@ -61,76 +61,84 @@ void make_sub_dir_path(
 }
 
 int is_relevant_dir(char dir_name[256]) {
-    if (dir_name[0] == '.' && dir_name[1] == '\0') {
-        return 0;
-    }
+	if (dir_name[0] == '.' && dir_name[1] == '\0') {
+		return 0;
+	}
 
-    if (dir_name[0] == '.' && dir_name[1] == '.' && dir_name[2] == '\0') {
-        return 0;
-    }
+	if (
+		dir_name[0] == '.' &&
+		dir_name[1] == '.' &&
+		dir_name[2] == '\0') {
 
-    return 1;
+		return 0;
+	}
+
+	return 1;
 }
 
 int is_elm_file(char file_name[256]) {
-    int size = 0;
-    for (; size < 256; ++size) {
-        if (file_name[size] == '\0') {
-            break;
-        }
-    }
-
-    // smallest Elm file name
-    // 12345 size
-    // 01234 index
-    // X.elm
-    return size > 4 && file_name[size - 1] == 'm' && file_name[size - 2] == 'l' && file_name[size - 3] == 'e' && file_name[size - 4] == '.';
+	int size = 0;
+	for (; size < 256; ++size) {
+		if (file_name[size] == '\0') {
+			break;
+		}
+	}
+	
+	// smallest Elm file name
+	// 12345 size
+	// 01234 index
+	// X.elm
+	return
+		size > 4 &&
+		file_name[size - 1] == 'm' &&
+		file_name[size - 2] == 'l' &&
+		file_name[size - 3] == 'e' &&
+		file_name[size - 4] == '.';
 }
 
 #define CODE_BUF_SIZE 5000000
 
 struct CodeBuffers {
-    char one[CODE_BUF_SIZE];
-    char two[CODE_BUF_SIZE];
-    uint32_t two_size;
-    uint32_t one_size;
+	char one[CODE_BUF_SIZE];
+	char two[CODE_BUF_SIZE];
+	uint32_t two_size;
+	uint32_t one_size;
 };
 
 struct CodeBuffers code_buffers;
 
 void no_trailing_space() {
-    int num_consecutive_space = 0;
+	int num_consecutive_space = 0;
 
-    int two_i = 0;
-    for (int one_i = 0; one_i < code_buffers.one_size; ++one_i) {
-        if (code_buffers.one[one_i] == ' ') {
-            num_consecutive_space++;
-        }
+	int two_i = 0;
+	for (int one_i = 0; one_i < code_buffers.one_size; ++one_i) {
+		if (code_buffers.one[one_i] == ' ') {
+			num_consecutive_space++;
+		}
 
-        if (
-		code_buffers.one[one_i] != ' ' &&
-		code_buffers.one[one_i] != '\n') {
+		if (
+			code_buffers.one[one_i] != ' ' &&
+			code_buffers.one[one_i] != '\n') {
 
-            num_consecutive_space = 0;
-        }
+			num_consecutive_space = 0;
+		}
 
-        if (code_buffers.one[one_i] == '\n' && num_consecutive_space > 0) {
-            two_i = two_i - num_consecutive_space;
-            num_consecutive_space = 0;
-        }
+		if (
+			code_buffers.one[one_i] == '\n' &&
+			num_consecutive_space > 0) {
 
-        code_buffers.two[two_i] = code_buffers.one[one_i];
-        two_i++;
-    }
+			two_i = two_i - num_consecutive_space;
+			num_consecutive_space = 0;
+		}
 
-    code_buffers.two_size = two_i;
+		code_buffers.two[two_i] = code_buffers.one[one_i];
+		two_i++;
+	}
+
+	code_buffers.two_size = two_i;
 }
 
-void identity_pass() {
-    for (int i = 0; i < code_buffers.one_size; ++i) {
-        code_buffers.two[i] = code_buffers.one[i];
-    }
-    code_buffers.two_size = code_buffers.one_size;
+void newline_after_bind() {
 }
 
 int format_file(char path[MAX_PATH]) {
@@ -160,6 +168,7 @@ int format_file(char path[MAX_PATH]) {
 	code_buffers.one_size = n;
 
 	no_trailing_space();
+	newline_after_bind();
 
 	FILE* handle_out = fopen(path, "w");
 	if (handle_out == NULL) {
@@ -198,7 +207,7 @@ int get_paths_from_fs_entry(DIR* d, char directory_path[MAX_PATH]) {
 
 		int format_error = format_file(file_path);
 		if (format_error != 0) {
-		    return format_error;
+			return format_error;
 		}
 	}
 	if (dir->d_type == DT_DIR && !is_relevant_dir(dir->d_name)) {
@@ -244,7 +253,7 @@ int main(int argc, char* argv[]) {
 	top_path[1] = '\0';
 	d = opendir(top_path);
 	if (d == NULL) {
-        printf("directory doesn't exist: %s", top_path);
+	printf("directory doesn't exist: %s", top_path);
 		return -1;
 	}
 	int result = get_paths_from_dir(d, top_path);
