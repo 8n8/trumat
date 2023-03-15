e5980bfc346777c27d98e863a725aeb5b22e878d
True Ghiassi <true@ghiassitrio.co.uk>
2023-03-15 21:26:01 +0000

    Removed the Haskell code

--- format.sh
+++
@@ -1,3 +0,0 @@
-ormolu -i src/*
-ormolu -i test/*
-ormolu -i app/*
--- src/Trumat.hs
+++
@@ -1,8 +0,0 @@
-module Trumat (format) where
-
-import Array8 (Array8)
-import Ast (Ast)
-
-format :: Array8 -> Ast -> IO (Either String ())
-format _ _ =
-  return $ Right ()
