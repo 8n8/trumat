75f1cba9c3ddb3547a6a94bff90cba78f840287b
True Ghiassi <true@ghiassitrio.co.uk>
2023-04-03 21:54:15 +0100

    Added a simple runtime

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,56 @@
 module Main (main) where
 
-import qualified Lib
-import qualified Prelude
+import Control.Exception (try)
+import Data.ByteString (readFile, writeFile)
+import Data.Text.Encoding (decodeUtf8', encodeUtf8)
+import Path (Path)
+import qualified Path
+import qualified System.Directory
+import Trumat (trumat)
+import Prelude
+  ( Either (Left, Right),
+    FilePath,
+    IO,
+    IOError,
+    Maybe (Just, Nothing),
+    mapM_,
+    putStrLn,
+    return,
+    ($),
+  )
 
-main :: Prelude.IO ()
-main = Lib.someFunc
+main :: IO ()
+main =
+  formatPath "."
+
+formatPath :: FilePath -> IO ()
+formatPath path =
+  do
+    eitherContents <- listDirectory path
+    case eitherContents of
+      Left _ ->
+        case Path.fromString path of
+          Nothing ->
+            return ()
+          Just elmPath ->
+            formatFile elmPath
+      Right contents ->
+        mapM_ formatPath contents
+
+listDirectory :: FilePath -> IO (Either IOError [FilePath])
+listDirectory path =
+  try $ System.Directory.listDirectory path
+
+formatFile :: Path -> IO ()
+formatFile path =
+  do
+    bytes <- readFile (Path.toString path)
+    case decodeUtf8' bytes of
+      Left _ ->
+        putStrLn "not a valid Elm file"
+      Right text ->
+        case trumat text of
+          Left err ->
+            putStrLn err
+          Right formatted ->
+            writeFile (Path.toString path) (encodeUtf8 formatted)
