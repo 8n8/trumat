3ff392f191424c6bf2533406f68e3b1b88d11584
True Ghiassi <true@ghiassitrio.co.uk>
2023-04-06 19:47:14 +0100

    Trying out pointer interface for formatters

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
+char* parse_literal(char* in, char* literal) {
+    char* i = in;
+    for (; *i == *literal && *literal != '\0'; ++i && ++literal) {
+    }
+
+    if (*i == '\0') {
+        return i;
+    }
+
+    return in;
+}
+
+char* copy_literal(char* out, char* literal) {
+    for (; *literal != '\0'; ++out && ++literal) {
+        *out = *literal;
+    }
+    return out;
+}
+
+int format_module_header(char** in, char** out) {
+    int result = parse_literal(*in, "module ");
+    if (result == in) {
+        return in;
+    }
+    out = copy_literal(out, "module ");
+    in = result;
+}
+
 int format(char* in, char* out) {
-    *out = '\0';
-    return 0;
+    int result = format_module_header(&in, &out);
+    if (result < 0) {
+        return result;
+    }
 }
