3e54bd8df07e0c73034e8bdf612a4b9e5a63f618
True Ghiassi <true@ghiassitrio.co.uk>
2023-05-02 21:06:55 +0100

    Left-to-right pipe operator: >>

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -922,7 +922,7 @@ parseInfix =
 
 infixes :: [Text]
 infixes =
-  ["<|", "++", "+", "|>"]
+  [">>", "<|", "++", "+", "|>"]
 
 parseInfixedExpression :: Int -> Int -> Parser Text
 parseInfixedExpression minColumn indent =
