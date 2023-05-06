84ca7e67b4a614a02f0f0e6b393fcd14ddd47b11
True Ghiassi <true@ghiassitrio.co.uk>
2023-05-06 13:02:30 +0100

    Function call between 'if' and 'else'.

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -1187,7 +1187,7 @@ parseIfThenElse indent =
     _ <- chunk "if"
     _ <- space1
     if_ <- parseExpression 1 DoesntNeedBrackets (indent + 4)
-    _ <- space1
+    _ <- space
     _ <- chunk "then"
     _ <- space1
     then_ <- parseExpression 1 DoesntNeedBrackets (indent + 4)
