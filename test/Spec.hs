94064d25fef722fa3e6d30d39700051f88a422f7
True Ghiassi <true@ghiassitrio.co.uk>
2023-06-16 22:48:28 +0100

    Revert "Multiline string in module without declaration."
    This reverts commit 8749a96b2dc6e89b71ac20d6899a16a1d895ad94.

--- test/Spec.hs
+++ test/Spec.hs
@@ -6935,19 +6935,5 @@ cases =
       \type alias X =\n\
       \    A\n\
       \"
-    ),
-    ( "multiline string in module without declaration",
-      "a = \"\"\"\n\
-      \b\n\
-      \\"\"\"\n\
-      \",
-      "module Main exposing (a)\n\
-      \\n\
-      \\n\
-      \a =\n\
-      \    \"\"\"\n\
-      \b\n\
-      \\"\"\"\n\
-      \"
     )
   ]
