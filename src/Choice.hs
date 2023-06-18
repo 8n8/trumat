da3254acba5ea4ab7e6938aec1fe832406c508a1
True Ghiassi <true@ghiassitrio.co.uk>
2023-06-18 10:04:59 +0100

    Removed everything prior to try a little experiment

--- src/Choice.hs
+++
@@ -1,6 +0,0 @@
-module Choice (Choice) where
-
-import ElmChar (ElmChar)
-
-data Choice a
-    = Choice [(ElmChar -> a -> a, a)]
--- src/Lib.hs
+++
@@ -1,9 +0,0 @@
-module Lib
-  ( someFunc,
-  )
-where
-
-import qualified Prelude
-
-someFunc :: Prelude.IO ()
-someFunc = Prelude.putStrLn "someFunc"
