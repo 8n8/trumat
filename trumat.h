78a290bd18289f8a5221053e3b0c3007ac957a24
True Ghiassi <true@ghiassitrio.co.uk>
2023-06-22 20:46:57 +0100

    Removed everything

--- trumat.h
+++
@@ -1,9 +0,0 @@
-#define BIG 1000000
-
-struct Memory {
-        uint16_t row_numbers[BIG];
-        uint16_t column_numbers[BIG];
-        uint8_t token
-}
-
-int format(char in[BIG], char out[BIG], struct Memory* memory);
