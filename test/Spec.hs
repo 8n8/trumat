26495d435b05697583110b82e8a63587f6724404
True Ghiassi <true@ghiassitrio.co.uk>
2023-07-11 21:44:37 +0100

    Hello world test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,51 @@
+module Main (main) where
+
+import qualified Test.Tasty.HUnit as HUnit
+import Test.Tasty
+import Data.Text (Text)
+import Trumat (format)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+    defaultMain tests
+
+
+tests :: TestTree
+tests = testGroup "Tests" (map oneTest cases)
+
+
+oneTest :: Case -> TestTree
+oneTest case_ =
+    HUnit.testCase (description case_) $
+        expected case_ HUnit.@?= format (input case_)
+
+
+data Case
+    = Case
+    { expected :: Either String Text
+    , description :: String
+    , input :: Text
+    }
+
+
+cases :: [Case]
+cases =
+    [ Case
+        { expected =
+            Right
+            "module X exposing (x)\n\
+            \\n\
+            \\n\
+            \x =\n\
+            \    0\n\
+            \",
+          description = "hello world formatted",
+          input =
+            "module X exposing (x)\n\
+            \\n\
+            \\n\
+            \x =\n\
+            \    0\n\
+            \"
+        }
+    ]
