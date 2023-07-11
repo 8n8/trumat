d760ee478750335664c21be84e417e3391c8c66c
True Ghiassi <true@ghiassitrio.co.uk>
2023-07-11 21:31:43 +0100

    Started over in Haskell.
    I should have used property tests from the start.

--- app/Main.hs
+++ app/Main.hs
@@ -1,60 +1,6 @@
 module Main (main) where
 
-import Control.Exception (try)
-import Data.ByteString (readFile, writeFile)
-import Data.Text.Encoding (decodeUtf8', encodeUtf8)
-import Path (Path)
-import qualified Path
-import qualified System.Directory
-import Trumat (trumat)
-import Prelude
-  ( Either (Left, Right),
-    FilePath,
-    IO,
-    IOError,
-    Maybe (Just, Nothing),
-    map,
-    mapM_,
-    putStrLn,
-    return,
-    ($),
-    (<>),
-  )
+import Lib
 
 main :: IO ()
-main =
-  formatPath "."
-
-formatPath :: FilePath -> IO ()
-formatPath path =
-  do
-    eitherContents <- listDirectory path
-    case eitherContents of
-      Left _ ->
-        case Path.fromString path of
-          Nothing ->
-            return ()
-          Just elmPath ->
-            do
-              putStrLn path
-              formatFile elmPath
-      Right contents ->
-        mapM_ formatPath $ map (\dir -> path <> "/" <> dir) contents
-
-listDirectory :: FilePath -> IO (Either IOError [FilePath])
-listDirectory path =
-  try $ System.Directory.listDirectory path
-
-formatFile :: Path -> IO ()
-formatFile path =
-  do
-    bytes <- readFile (Path.toString path)
-    case decodeUtf8' bytes of
-      Left _ ->
-        putStrLn "not a valid Elm file"
-      Right text ->
-        case trumat text of
-          Left err ->
-            putStrLn err
-          Right formatted ->
-            writeFile (Path.toString path) (encodeUtf8 formatted)
+main = someFunc
