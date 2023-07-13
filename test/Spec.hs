ac5c83ddc42e9a3c61e12a5c02595357eb91a1fc
True Ghiassi <true@ghiassitrio.co.uk>
2023-07-13 21:51:59 +0100

    Added trivial test case.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+module Main (main) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+    defaultMain tests
+
+
+tests :: TestTree
+tests = testGroup "Tests"
+    [ testCase "2+2=4" $
+        2+2 @?= 4
+    ]
