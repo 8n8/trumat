ff27632a4604d6d6fedd656aa8074a510ec11146
True Ghiassi <true@ghiassitrio.co.uk>
2023-07-13 21:55:45 +0100

    Set up red test for hello world formatted.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,15 +2,34 @@ module Main (main) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
-
+import Trumat
 
 main :: IO ()
 main =
-    defaultMain tests
-
+  defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests"
+tests =
+  testGroup
+    "Tests"
     [ testCase "2+2=4" $
-        2+2 @?= 4
+        2 + 2 @?= 4,
+      testCase "hello world formatted" $
+        (@?=)
+          ( Trumat.format
+              "module X exposing (x)\n\
+              \\n\
+              \\n\
+              \x =\n\
+              \    0\n\
+              \"
+          )
+          ( Right
+              "module X exposing (x)\n\
+              \\n\
+              \\n\
+              \x =\n\
+              \    0\n\
+              \"
+          )
     ]
