f530e56cb4ad02b8c6ab71e536ae945d74b684ac
True Ghiassi <true@ghiassitrio.co.uk>
2023-07-27 19:49:35 +0100

    Partial tokeniser

--- trumat.c
+++ trumat.c
@@ -1,25 +1,25 @@
 #include "trumat.h"
 
 enum elm_char {
-    ch_0,
-    ch_equals,
-    ch_newline,
-    ch_close_parens,
-    ch_open_parens,
-    ch_g,
-    ch_n,
-    ch_i,
-    ch_s,
-    ch_o,
-    ch_p,
-    ch_x,
-    ch_X,
-    ch_space,
-    ch_e,
-    ch_l,
-    ch_u,
-    ch_d,
-    ch_m,
+    char_0,
+    char_equals,
+    char_newline,
+    char_close_parens,
+    char_open_parens,
+    char_g,
+    char_n,
+    char_i,
+    char_s,
+    char_o,
+    char_p,
+    char_x,
+    char_X,
+    char_space,
+    char_e,
+    char_l,
+    char_u,
+    char_d,
+    char_m,
 };
 
 void zero_memory(struct Memory* memory) {
@@ -93,6 +93,17 @@ static int parse_char(uint8_t raw) {
     return -1;
 }
 
+static int u8x1m_append(enum elm_char ch, struct u8x1m* chars) {
+    if (chars->length == 1000000) {
+        return -1;
+    }
+
+    chars->items[chars->length] = ch;
+    ++chars->length;
+
+    return 0;
+}
+
 static int parse_chars(struct u8x1m* raw, struct u8x1m* chars) {
     for (int i = 0; ; ++i) {
         int result = get_u8x1m(raw, i);
@@ -104,7 +115,461 @@ static int parse_chars(struct u8x1m* raw, struct u8x1m* chars) {
         if (result < 0) {
             return result;
         }
+
+        result = u8x1m_append(result, chars);
+        if (result != 0) {
+            return result;
+        }
     }
+}
+
+enum tokeniser {
+    tokeniser_start,
+    tokeniser_m,
+    tokeniser_l,
+    tokeniser_e,
+    tokeniser_upper_name,
+    tokeniser_p,
+    tokeniser_o,
+    tokeniser_i,
+    tokeniser_lower_name,
+    tokeniser_0,
+};
+
+// reserved keywords in Elm: as case else exposing if import in let module
+// of port then type where
+static int tokeniser_state_machine(enum tokeniser state, enum elm_char ch) {
+    switch (state) {
+    case tokeniser_upper_name:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_upper_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_upper_name;
+        case char_n:
+            return tokeniser_upper_name;
+        case char_i:
+            return tokeniser_upper_name;
+        case char_s:
+            return tokeniser_upper_name;
+        case char_o:
+            return tokeniser_upper_name;
+        case char_p:
+            return tokeniser_upper_name;
+        case char_x:
+            return tokeniser_upper_name;
+        case char_X:
+            return tokeniser_upper_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_upper_name;
+        case char_l:
+            return tokeniser_el;
+        case char_m:
+            return tokeniser_upper_name;
+        case char_d:
+            return tokeniser_upper_name;
+        case char_u:
+            return tokeniser_upper_name;
+        };
+    case tokeniser_e:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_lower_name;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_ex;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_le;
+        case char_l:
+            return tokeniser_el;
+        case char_m:
+            return tokeniser_lower_name;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_l:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_lower_name;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_le;
+        case char_l:
+            return tokeniser_lower_name;
+        case char_m:
+            return tokeniser_lower_name;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_m:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_mo;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_lower_name;
+        case char_l:
+            return tokeniser_lower_name;
+        case char_m:
+            return tokeniser_lower_name;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_p:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_po;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_lower_name;
+        case char_l:
+            return tokeniser_lower_name;
+        case char_m:
+            return tokeniser_lower_name;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_o:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_lower_name;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_lower_name;
+        case char_l:
+            return tokeniser_lower_name;
+        case char_m:
+            return tokeniser_lower_name;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_i:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_in;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_lower_name;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_lower_name;
+        case char_l:
+            return tokeniser_lower_name;
+        case char_m:
+            return tokeniser_im;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_lower_name:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_lower_name;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_lower_name;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_lower_name;
+        case char_p:
+            return tokeniser_lower_name;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_lower_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_lower_name;
+        case char_l:
+            return tokeniser_lower_name;
+        case char_m:
+            return tokeniser_lower_name;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    case tokeniser_0:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return -1;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return -1;
+        case char_n:
+            return -1;
+        case char_i:
+            return -1;
+        case char_s:
+            return -1;
+        case char_o:
+            return -1;
+        case char_p:
+            return -1;
+        case char_x:
+            return tokeniser_0x;
+        case char_X:
+            return -1;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return -1;
+        case char_l:
+            return -1;
+        case char_m:
+            return -1;
+        case char_d:
+            return -1;
+        case char_u:
+            return -1;
+        };
+    case tokeniser_start:
+        switch (ch) {
+        case char_equals:
+            return tokeniser_start;
+        case char_0:
+            return tokeniser_0;
+        case char_newline:
+            return tokeniser_start;
+        case char_close_parens:
+            return tokeniser_start;
+        case char_open_parens:
+            return tokeniser_start;
+        case char_g:
+            return tokeniser_lower_name;
+        case char_n:
+            return tokeniser_lower_name;
+        case char_i:
+            return tokeniser_i;
+        case char_s:
+            return tokeniser_lower_name;
+        case char_o:
+            return tokeniser_o;
+        case char_p:
+            return tokeniser_p;
+        case char_x:
+            return tokeniser_lower_name;
+        case char_X:
+            return tokeniser_upper_name;
+        case char_space:
+            return tokeniser_start;
+        case char_e:
+            return tokeniser_e;
+        case char_l:
+            return tokeniser_l;
+        case char_m:
+            return tokeniser_m;
+        case char_d:
+            return tokeniser_lower_name;
+        case char_u:
+            return tokeniser_lower_name;
+        };
+    };
+
+    return -1;
+}
+
+static int make_tokeniser_states(struct u8x1m chars, struct u8x1m* states) {
+    enum tokeniser state = tokeniser_start;
+    for (int i = 0; i < chars.length; ++i) {
+        enum elm_char ch = chars.items[i];
+        int result = tokeniser_state_machine(state, ch);
+        if (result < 0) {
+            return result;
+        }
+        result = u8x1m_append(result, states);
+        if (result != 0) {
+            return result;
+        }
+    }
+
+    return 0;
 }
 
 int format(FILE* input_file, FILE* output_file, struct Memory* memory) {
@@ -113,5 +578,10 @@ int format(FILE* input_file, FILE* output_file, struct Memory* memory) {
         return result;
     }
 
-    return parse_chars(&memory->raw, &memory->chars);
+    result = parse_chars(&memory->raw, &memory->chars);
+    if (result != 0) {
+        return result;
+    }
+
+    return make_tokeniser_states(memory->chars, &memory->tokeniser_state);
 }
--- trumat.h
+++ trumat.h
@@ -21,16 +21,20 @@ struct u16x500k {
     uint16_t items[500000];
 };
 
-struct tokens {
-    struct u8x500k token_type;
-    struct u32x500k token_start;
-    struct u16x500k token_size;
-};
-
 struct Memory {
     struct u8x1m raw;
     struct u8x1m chars;
-    struct tokens tokens;
+
+    // These are used during tokenisation.
+    struct u8x1m tokeniser_state;
+    struct u8x1m num_tokens;
+    struct u8x1m first_token;
+    struct u8x1m second_token;
+
+    // These are the result of tokenisation.
+    struct u8x500k token_type;
+    struct u32x500k token_start;
+    struct u16x500k token_size;
 };
 
 void zero_memory(struct Memory*);
