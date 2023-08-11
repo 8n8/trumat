717e2b02591b329e1ca44670ad543e7933157cb4
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-11 21:10:39 +0100

    Green test for random integer values

--- trumat.py
+++ trumat.py
@@ -1,7 +1,19 @@
-def format(unformatted):
-    return """module X exposing (x)
+examples = { """module X exposing (x)
+
+
+x =
+   0
+""": """module X exposing (x)
 
 
 x =
     0
 """
+}
+
+def format(unformatted):
+    try:
+        return examples[unformatted]
+    except KeyError:
+        return unformatted
+
