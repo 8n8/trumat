56df9ebd4a36bd3c912b1e6f0cdde86ff4420b60
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-11 20:22:44 +0100

    Red test for hello world unformatted

--- trumat_test.py
+++ trumat_test.py
@@ -1,18 +1,41 @@
 from hypothesis import given, strategies as st
 import trumat
 
-def test_hello_world():
-    input = """module X exposing (x)
+formatteds = [
+    """module X exposing (x)
 
 
 x =
     0
 """
-    assert trumat.format(input) == input
+]
 
 
-def test_silly():
-    assert 1 == 1
+def test_formatted():
+    for formatted in formatteds:
+        assert trumat.format(formatted) == formatted
+
+
+unformatteds = [
+    {
+        "input": """module X exposing (x)
+
+
+x =
+   0
+""",
+        "expected": """module X exposing (x)
+
+
+x =
+    0""",
+    }
+]
+
+
+def test_unformatted():
+    for unformatted in unformatteds:
+        assert trumat.format(unformatted["input"]) == unformatted["expected"]
 
 
 @given(x=st.integers(), y=st.integers())
