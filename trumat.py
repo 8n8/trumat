8d50c81033ee205998471e6a6536f41fbef646cd
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-12 13:26:00 +0100

    Refactoring

--- trumat.py
+++ trumat.py
@@ -1,21 +1,4 @@
-examples = { """module X exposing (x)
-
-
-x =
-   0
-""": """module X exposing (x)
-
-
-x =
-    0
-"""
-}
-
 def format(unformatted):
-    try:
-        return examples[unformatted]
-    except KeyError:
-        [beforeEquals, afterEquals] = unformatted.split("=")
-        return f"""{beforeEquals}=
-    {afterEquals.strip()}
+    return f"""{unformatted.split("=")[0]}=
+    {unformatted.split("=")[1].strip()}
 """
