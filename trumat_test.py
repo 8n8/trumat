9b8a8184139e0baaacd188f3967619070037af0e
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-18 20:31:20 +0100

    A basic unformatted test

--- trumat_test.py
+++ trumat_test.py
@@ -9,7 +9,26 @@ x =
 """
 ]
 
+unformatteds = [
+    ( """module X exposing (x)
+
+
+x =
+   0
+""",
+    """module X exposing (x)
+
+
+x =
+    0
+""")
+]
+
 
 def test_formatted():
     for formatted in formatteds:
         assert trumat.format(formatted) == formatted
+
+def test_unformatted():
+    for unformatted, formatted in unformatteds:
+        assert trumat.format(unformatted) == formatted
