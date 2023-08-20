c66866ef1ea2da4bd72322a04c95ccb39db4517d
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-20 18:19:01 +0100

    Fail if module keyword is not in first column

--- trumat.py
+++ trumat.py
@@ -1,7 +1,30 @@
 #!/bin/python3
 
-def format(unformatted):
-    return unformatted
+def parse_column_numbers(code):
+    column_numbers = [0] * len(code)
+    column_number = 0
+    for i, c in enumerate(code):
+        column_numbers[i] = column_number
+
+        if c == '\n':
+            column_number = 0
+        else:
+            column_number += 1
+
+
+    return column_numbers
+
+def format(code):
+    columns = parse_column_numbers(code)
+
+    i = 0
+    while code[i] == '\n' or code[i] == ' ':
+        i += 1
+
+    if columns[i] != 0:
+        raise ValueError("should be first column")
+
+    return code
 
 def elm_paths():
     elm_paths = []
@@ -18,10 +41,10 @@ def main():
     for elm_path in elm_paths():
         print(elm_path)
         with open(elm_path, "rb") as file_handle:
-            unformatted = file_handle.read()
+            code = file_handle.read()
 
         with open(elm_path, "wb") as file_handle:
-            file_handle.write(format(unformatted, db))
+            file_handle.write(format(code, db))
 
 
 if __name__ == "__main__":
--- trumat_test.py
+++ trumat_test.py
@@ -8,3 +8,16 @@ x =
     0
 """
     assert trumat.format(input) == input
+
+def test_spaces_before_module_keyword():
+    input = """ module X exposing (x)
+
+
+x =
+    0
+"""
+    try:
+        trumat.format(input)
+        assert False
+    except ValueError:
+        assert True
