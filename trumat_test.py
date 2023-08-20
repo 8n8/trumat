ad97857d35e442ee968e1bcee75d1162fe739ad8
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-20 17:43:34 +0100

    Hello world formatter

--- trumat_test.py
+++ trumat_test.py
@@ -1,42 +1,10 @@
 import trumat
 
-formatteds = [
-    b"""module X exposing (x)
+def test_hello_world_formatted():
+    input = """module X exposing (x)
 
 
 x =
     0
-""",
-    b"""module X exposing (x)
-
-
-x =
-    []
 """
-]
-
-unformatteds = [
-    ( b"""module X exposing (x)
-
-
-x =
-   0
-""",
-    b"""module X exposing (x)
-
-
-x =
-    0
-""")
-]
-
-
-def test_formatted():
-    db = trumat.init_db()
-    for formatted in formatteds:
-        assert trumat.format(formatted, db) == formatted
-
-def test_unformatted():
-    db = trumat.init_db()
-    for unformatted, formatted in unformatteds:
-        assert trumat.format(unformatted, db) == formatted
+    assert trumat.format(input) == input
