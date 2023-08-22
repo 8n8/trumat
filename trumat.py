9b90f3e1777f647b88fab559207fcfe54ff51ff4
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-22 17:37:22 +0100

    Trying out a branchless approach. It doesn't work.

--- trumat.py
+++ trumat.py
@@ -1,6 +1,25 @@
 #!/bin/python3
 
 def format(code):
+    equals_before = [False] * len(code)
+    equals_before_state = False
+    for i, c in enumerate(code):
+        equals_before[i] = equals_before_state
+        equals_before_state = c == "="
+
+    equals_before1 = [False] * len(code)
+    equals_before_state1 = False
+    for i, c in enumerate(equals_before):
+        equals_before1[i] = equals_before_state1
+        equals_before_state1 = c
+
+    newline_before = [False] * len(code)
+    newline_before_state = False
+    for i, c in enumerate(code):
+        newline_before[i] = newline_before_state
+        newline_before_state = c == "\n"
+
+
     return code
 
 def elm_paths():
@@ -14,14 +33,13 @@ def elm_paths():
     return elm_paths
 
 def main():
-    db = init_db()
     for elm_path in elm_paths():
         print(elm_path)
         with open(elm_path, "rb") as file_handle:
             code = file_handle.read()
 
         with open(elm_path, "wb") as file_handle:
-            file_handle.write(format(code, db))
+            file_handle.write(format(code))
 
 
 if __name__ == "__main__":
