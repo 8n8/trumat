6855b2f0a2719cfade0f137bb573fcaa3fc538f1
True Ghiassi <true@ghiassitrio.co.uk>
2023-08-22 17:37:35 +0100

    Revert "Trying out a branchless approach. It doesn't work."
    This reverts commit 9b90f3e1777f647b88fab559207fcfe54ff51ff4.

--- trumat_test.py
+++ trumat_test.py
@@ -1,21 +1,6 @@
 import trumat
 import hypothesis
 
-def test_hello_world_unformatted():
-    input = """module X exposing (x)
-
-
-x =
-   0
-"""
-    expected = """module X exposing (x)
-
-
-x =
-    0
-"""
-    assert trumat.format(input) == expected
-
 def test_hello_world():
     input = """module X exposing (x)
 
