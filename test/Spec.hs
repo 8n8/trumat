af37e0665d47b30540d8912227e1c6086f33f5a2
True Ghiassi <true@ghiassitrio.co.uk>
2023-09-21 19:56:27 +0100

    Red test for if then else preceded by less than operator

--- test/Spec.hs
+++ test/Spec.hs
@@ -12088,5 +12088,25 @@ cases =
       \                d\n\
       \           )\n\
       \"
+    ),
+    ( "if then else preceded by less than operator",
+      "module X exposing (x)\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    a < if b then c else d\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    a\n\
+      \        < (if b then\n\
+      \            c\n\
+      \\n\
+      \           else\n\
+      \            d\n\
+      \          )\n\
+      \"
     )
   ]
