541f20944fe92b2d5e8f38207cb80a60c7f2354d
True Ghiassi <true@ghiassitrio.co.uk>
2023-09-29 20:00:11 +0100

    Escaped backslash in unordered list

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -194,7 +194,7 @@ parseUnorderedListItemHelp nesting indent accumulated isGappy =
           ( \s ->
               if s == "*"
                 then "*"
-                else escapeAsterisks s
+                else (escapeAsterisks . escapeBackslashes) s
           )
           $ fmap Text.strip
           $ fmap (\line -> Text.intercalate " " $ Text.words line)
@@ -639,6 +639,7 @@ parseEscapeBackslashes =
         choice
           [ takeWhile1P Nothing (\ch -> ch /= '\\'),
             chunk "\\\\",
+            chunk "\\*",
             do
               backslashes <- takeWhile1P Nothing (\ch -> ch == '\\')
               return $ Text.replace "\\" "\\\\" backslashes,
