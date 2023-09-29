950c20ebd2c1bc43c11e7aa96c418b093586631d
True Ghiassi <true@ghiassitrio.co.uk>
2023-09-29 19:49:19 +0100

    Red test for escaped backslash in unordered list.

--- test/Spec.hs
+++ test/Spec.hs
@@ -12601,5 +12601,31 @@ cases =
       \x =\n\
       \    0\n\
       \"
+    ),
+    ( "escaped backslash in unordered list",
+      "module X exposing (x)\n\
+      \\n\
+      \{-|\n\
+      \\n\
+      \  - \\\n\
+      \\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \{-|\n\
+      \\n\
+      \  - \\\\\n\
+      \\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \"
     )
   ]
