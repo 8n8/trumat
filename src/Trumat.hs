c7075ed98384a84857c3bb686c45daa6b35684d4
True Ghiassi <true@ghiassitrio.co.uk>
2023-09-30 20:22:23 +0100

    Single backtick in unordered list item

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -194,7 +194,7 @@ parseUnorderedListItemHelp nesting indent accumulated isGappy =
           ( \s ->
               if s == "*"
                 then "*"
-                else (escapeAsterisks . escapeBackslashes) s
+                else (escapeBackticks . escapeAsterisks . escapeBackslashes) s
           )
           $ fmap Text.strip
           $ fmap (\line -> Text.intercalate " " $ Text.words line)
