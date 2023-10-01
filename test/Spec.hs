ecba777d787cb2c5620c6da015fd2db8eb2216dd
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 14:59:08 +0100

    Red test for beshare URL in doc comment

--- test/Spec.hs
+++ test/Spec.hs
@@ -13159,5 +13159,25 @@ cases =
       \x =\n\
       \    0\n\
       \"
+    ),
+    ( "beshare url in doc comment",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| beshare:a\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| <beshare:a>\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \"
     )
   ]
