67d69e5571e9a585c181532948f67fe2e14f77fb
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 14:51:18 +0100

    Red test for attachment URL in doc comment

--- test/Spec.hs
+++ test/Spec.hs
@@ -13119,5 +13119,25 @@ cases =
       \x =\n\
       \    0\n\
       \"
+    ),
+    ( "attachment URL in doc comment",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| attachment:a\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| <attachment:a>\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \"
     )
   ]
