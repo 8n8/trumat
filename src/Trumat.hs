7eb6b8c1acc41e189f0cdca950aaaecd6479b9f4
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 16:10:30 +0100

    crid URL in doc comment

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -657,7 +657,7 @@ angleBracketUrls text =
 
 urlSchemes :: Set Text
 urlSchemes =
-  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare", "bitcoin", "bolo", "callto", "cap", "chrome", "cid", "coap", "content"]
+  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare", "bitcoin", "bolo", "callto", "cap", "chrome", "cid", "coap", "content", "crid"]
 
 angleBracketUrl :: Text -> Text
 angleBracketUrl text =
