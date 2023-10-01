56e3888e045b47b2dc001d932348dcff17b4a140
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 15:06:09 +0100

    Bolo URL in doc comment

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -657,7 +657,7 @@ angleBracketUrls text =
 
 urlSchemes :: Set Text
 urlSchemes =
-  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare", "bitcoin"]
+  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare", "bitcoin", "bolo"]
 
 angleBracketUrl :: Text -> Text
 angleBracketUrl text =
