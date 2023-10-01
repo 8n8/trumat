efdca572d1c52ab2cef0972edf0389c7d21719c6
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 14:30:37 +0100

    afp URL in doc comment

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -657,7 +657,7 @@ angleBracketUrls text =
 
 urlSchemes :: Set Text
 urlSchemes =
-  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra"]
+  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp"]
 
 angleBracketUrl :: Text -> Text
 angleBracketUrl text =
