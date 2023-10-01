575d1e403ac3a5a034fd20edd44599bf4a8c9166
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 15:03:16 +0100

    bitcoin URL in doc comment

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -657,7 +657,7 @@ angleBracketUrls text =
 
 urlSchemes :: Set Text
 urlSchemes =
-  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare"]
+  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare", "bitcoin"]
 
 angleBracketUrl :: Text -> Text
 angleBracketUrl text =
