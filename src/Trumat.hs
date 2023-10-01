aae68ffaf9eb216382e65488da38c17fc5751813
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 14:52:17 +0100

    Attachment URL in doc comment

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -657,7 +657,7 @@ angleBracketUrls text =
 
 urlSchemes :: Set Text
 urlSchemes =
-  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt"]
+  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment"]
 
 angleBracketUrl :: Text -> Text
 angleBracketUrl text =
