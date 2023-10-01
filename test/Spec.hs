36518763fa4718364e7c4cbf6cdc64e7a16711a8
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 16:14:28 +0100

    Data URL in doc comment

--- test/Spec.hs
+++ test/Spec.hs
@@ -13379,5 +13379,25 @@ cases =
       \x =\n\
       \    0\n\
       \"
+    ),
+    ( "data url in doc comment",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| data:a\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| <data:a>\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \"
     )
   ]
