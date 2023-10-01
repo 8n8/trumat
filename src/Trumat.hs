67f9af6948e9039e8a613d75c81f26d3685b7c62
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 15:00:38 +0100

    Beshare URL in doc comment

--- src/Trumat.hs
+++ src/Trumat.hs
@@ -657,7 +657,7 @@ angleBracketUrls text =
 
 urlSchemes :: Set Text
 urlSchemes =
-  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw"]
+  Set.fromList ["aaa", "http", "https", "acap", "adiumxtra", "afp", "afs", "aim", "apt", "attachment", "aw", "beshare"]
 
 angleBracketUrl :: Text -> Text
 angleBracketUrl text =
