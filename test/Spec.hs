8da9718150c28940e3b5f786fd3a05427fca87fb
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-01 18:49:04 +0100

    Red test for link alias in doc comment

--- test/Spec.hs
+++ test/Spec.hs
@@ -13499,5 +13499,31 @@ cases =
       \x =\n\
       \    0\n\
       \"
+    ),
+    ( "link alias in doc comment",
+      "module X exposing (x)\n\
+      \\n\
+      \{-|\n\
+      \\n\
+      \[a]: b\n\
+      \\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \{-|\n\
+      \\n\
+      \[a]: b\n\
+      \\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \"
     )
   ]
