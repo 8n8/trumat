184aa40ac8b1fceea91e0b82bdf694f5142812ad
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-05 21:04:39 +0100

    Red test for underline style second level header

--- test/Spec.hs
+++ test/Spec.hs
@@ -14145,5 +14145,30 @@ cases =
       \x =\n\
       \    0\n\
       \"
+    ),
+    ( "underline second level header",
+      "module X exposing (x)\n\
+      \\n\
+      \{-| a\n\
+      \--\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \",
+      "module X exposing (x)\n\
+      \\n\
+      \{-|\n\
+      \\n\
+      \\n\
+      \## a\n\
+      \\n\
+      \-}\n\
+      \\n\
+      \\n\
+      \x =\n\
+      \    0\n\
+      \"
     )
   ]
