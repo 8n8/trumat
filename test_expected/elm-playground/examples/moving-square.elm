7c1ae5544c2b35ec81fbf4ffcbbed93bf4bda5f7
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-23 22:15:07 +0100

    Added elm-playground to the regression tests

---
+++ test_expected/elm-playground/examples/moving-square.elm
@@ -0,0 +1,19 @@
+module Main exposing (main, update, view)
+
+import Playground exposing (..)
+
+
+main =
+    game view update ( 0, 0 )
+
+
+view computer ( x, y ) =
+    [ square blue 40
+        |> move x y
+    ]
+
+
+update computer ( x, y ) =
+    ( x + toX computer.keyboard
+    , y + toY computer.keyboard
+    )
