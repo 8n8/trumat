5df8cf8d108710b4660f95697f3414704a9c89a7
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-27 21:41:30 +0100

    Minor refactoring

--- test.c
+++ test.c
@@ -8,12 +8,13 @@ void run_tests(char *);
 int main(int argc, char *argv[]) { run_tests("test_input"); }
 
 void run_tests(char *path) {
-  DIR *directory;
+  DIR *directory = opendir(path);
   struct dirent *item_in_directory;
-  directory = opendir(path);
   if (directory != NULL) {
-    while ((item_in_directory = readdir(directory)) != NULL) {
+    item_in_directory = readdir(directory);
+    while (item_in_directory != NULL) {
       puts(item_in_directory->d_name);
+      item_in_directory = readdir(directory);
     }
     closedir(directory);
     return;
