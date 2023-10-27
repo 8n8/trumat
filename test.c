0d3364118d7fd5e5e7df19e15d715cba3710e1a1
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-27 21:38:01 +0100

    Factored out test runner into separate function

--- test.c
+++ test.c
@@ -3,19 +3,23 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+void run_tests(char*);
 
 int main(int argc, char* argv[]) {
+  run_tests("test_input");
+}
+
+void run_tests(char* path) {
   DIR* directory;
   struct dirent *item_in_directory;
-  directory = opendir("./");
+  directory = opendir(path);
   if (directory != NULL) {
     while ((item_in_directory = readdir(directory)) != NULL) {
       puts(item_in_directory->d_name);
     }
     closedir(directory);
-    return 0;
+    return;
   } else {
     perror("Couldn't open the directory");
-    return -1;
   }
 }
