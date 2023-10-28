286d4ee37565277479dae5ce26567a0b464effce
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-28 16:44:51 +0100

    Copy input to output

--- trumat.c
+++ trumat.c
@@ -1,10 +1,10 @@
 #include "trumat.h"
 
-
-void zero_memory(struct memory* m) {
-}
-
+void zero_memory(struct memory *m) {}
 
 int format(uint8_t in[1000000], uint8_t out[1000000], struct memory *m) {
-  return -1;
+  for (int i = 0; i < 1000000; ++i) {
+    out[i] = in[i];
+  }
+  return 0;
 }
