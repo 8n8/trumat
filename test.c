da424685dde1d9280fb06312fd517b6abb79fdb8
True Ghiassi <true@ghiassitrio.co.uk>
2023-10-28 20:58:39 +0100

    Test runner for unchanged code

--- test.c
+++ test.c
@@ -5,8 +5,12 @@
 
 void run_positive_tests(char *, uint8_t in[1000000], uint8_t out[1000000],
                         struct memory *);
-void run_one_test(char *, uint8_t in[1000000], uint8_t out[1000000],
-                  struct memory *);
+void run_no_change_tests(char *, uint8_t in[1000000], uint8_t out[1000000],
+                         struct memory *);
+void run_one_positive_test(char *, uint8_t in[1000000], uint8_t out[1000000],
+                           struct memory *);
+void run_one_no_change_test(char *, uint8_t in[1000000], uint8_t out[1000000],
+                            struct memory *);
 int is_elm_path(char *);
 void make_sub_path(char *, char *, char *);
 int is_dot_path(char *);
@@ -63,6 +67,34 @@ struct memory MEMORY;
 
 int main(int argc, char *argv[]) {
   run_positive_tests("test_input", IN, OUT, &MEMORY);
+  zero_memory(&MEMORY);
+  run_no_change_tests("test_formatted", IN, OUT, &MEMORY);
+}
+
+void run_no_change_tests(char *path, uint8_t in[1000000], uint8_t out[1000000],
+                         struct memory *memory) {
+  DIR *directory = opendir(path);
+  struct dirent *item_in_directory;
+  if (directory != NULL) {
+    item_in_directory = readdir(directory);
+    while (item_in_directory != NULL) {
+      if (!is_dot_path(item_in_directory->d_name)) {
+        char sub_path[256];
+        make_sub_path(path, item_in_directory->d_name, sub_path);
+        run_no_change_tests(sub_path, in, out, memory);
+      }
+      item_in_directory = readdir(directory);
+    }
+    closedir(directory);
+    return;
+  }
+
+  if (!is_elm_path(path)) {
+    return;
+  }
+
+  zero_memory(memory);
+  run_one_no_change_test(path, in, out, memory);
 }
 
 void run_positive_tests(char *path, uint8_t in[1000000], uint8_t out[1000000],
@@ -88,7 +120,7 @@ void run_positive_tests(char *path, uint8_t in[1000000], uint8_t out[1000000],
   }
 
   zero_memory(memory);
-  run_one_test(path, in, out, memory);
+  run_one_positive_test(path, in, out, memory);
 }
 
 int string_length(char *path) {
@@ -104,8 +136,56 @@ int is_elm_path(char *path) {
          path[length - 3] == 'e' && path[length - 4] == '.';
 }
 
-void run_one_test(char *in_path, uint8_t in[1000000], uint8_t out[1000000],
-                  struct memory *memory) {
+void run_one_no_change_test(char *in_path, uint8_t in[1000000],
+                            uint8_t out[1000000], struct memory *memory) {
+  FILE *in_file = fopen(in_path, "rb");
+  if (in_file == NULL) {
+    char error_message[256];
+    sprintf(error_message, "could not open file: %s", in_path);
+    print_error(in_path, error_message);
+    return;
+  }
+  int in_size = fread(in, 1, 1000000, in_file);
+  in[in_size] = 0;
+
+  int result = format(in, out, memory);
+
+  if (result != 0) {
+    char error_message[256];
+    sprintf(error_message, "formatter failed with non-zero result: %d", result);
+    print_error(in_path, error_message);
+    return;
+  }
+
+  for (int i = 0; i < 1000000; ++i) {
+    if (in[i] == 0 && out[i] == 0) {
+      break;
+    }
+
+    if (in[i] == 0) {
+      print_error(in_path, "output is too long");
+      return;
+    }
+
+    if (out[i] == 0) {
+      print_error(in_path, "output is too short");
+      return;
+    }
+
+    if (in[i] != out[i]) {
+      char error_message[300];
+      sprintf(error_message, "expected '%c' but got '%c' at position %d", in[i],
+              out[i], i);
+      print_error(in_path, error_message);
+      return;
+    }
+  }
+
+  printf("SUCCESS: %s\n", in_path);
+}
+
+void run_one_positive_test(char *in_path, uint8_t in[1000000],
+                           uint8_t out[1000000], struct memory *memory) {
   FILE *in_file = fopen(in_path, "rb");
   if (in_file == NULL) {
     char error_message[256];
