0a4e86b07b6a4c2cbc5c0103efed62938478f62c
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-04 15:56:48 +0000

    Function for appending ASCII to text

--- text.c
+++ text.c
@@ -27,6 +27,30 @@ static int copy_to_head(struct text t, struct text_memory *m) {
   return 0;
 }
 
+int text_append_ascii(struct text left, const char * right, struct text* result, struct text_memory* m) {
+
+  if (left.end == m->head) {
+    result->start = left.start;
+  } else {
+    result->start = m->head;
+  }
+
+  if (left.end != m->head) {
+    if (copy_to_head(left, m)) {
+      return -1;
+    }
+  }
+
+  for (int i = 0; right[i] != '\0'; ++i) {
+    if (append_char(right[i], m)) {
+      return -1;
+    }
+  }
+
+  result->end = m->head;
+  return 0;
+}
+
 int text_join(struct text left, struct text right, struct text *result,
               struct text_memory *m) {
   if (left.end == m->head) {
