ae4f92175e51099f6d0c9bf5184c9c954b35d5a2
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-04 14:42:34 +0000

    Proper bounds checks on text

--- text.c
+++ text.c
@@ -2,29 +2,38 @@
 
 void text_zero_memory(struct text_memory *m) { m->head = 0; }
 
-int text_append_ascii_char(struct text left, char right, struct text *result,
-                           struct text_memory *m) {
-
+static int append_char(char ch, struct text_memory *m) {
   if (m->head == TEXT_SIZE) {
     return -1;
   }
 
+  m->bytes[m->head] = ch;
+  ++m->head;
+  return 0;
+}
+
+int text_append_ascii_char(struct text left, char right, struct text *result,
+                           struct text_memory *m) {
+
   if (left.end == m->head) {
     result->start = left.start;
-    m->bytes[m->head] = right;
-    ++m->head;
+    if (append_char(right, m)) {
+      return -1;
+    }
     result->end = m->head;
     return 0;
   }
 
   result->start = m->head;
   for (int i = left.start; i < left.end; ++i) {
-    m->bytes[m->head] = m->bytes[left.start + i];
-    ++m->head;
+    if (append_char(m->bytes[left.start + i], m)) {
+      return -1;
+    }
   }
 
-  m->bytes[m->head] = right;
-  ++m->head;
+  if (append_char(right, m)) {
+    return -1;
+  }
   result->end = m->head;
 
   return 0;
