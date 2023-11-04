0af5497cb44e6cc23fc58467eb3efb3487577897
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-04 14:28:31 +0000

    Better variable names

--- text.c
+++ text.c
@@ -2,28 +2,28 @@
 
 void text_zero_memory(struct text_memory *m) { m->head = 0; }
 
-int text_append_ascii_char(struct text base, char ch, struct text *result,
+int text_append_ascii_char(struct text left, char right, struct text *result,
                            struct text_memory *m) {
 
   if (m->head == TEXT_SIZE) {
     return -1;
   }
 
-  if (base.end == m->head) {
-    result->start = base.start;
-    m->bytes[m->head] = ch;
+  if (left.end == m->head) {
+    result->start = left.start;
+    m->bytes[m->head] = right;
     ++m->head;
     result->end = m->head;
     return 0;
   }
 
   result->start = m->head;
-  for (int i = base.start; i < base.end; ++i) {
-    m->bytes[m->head] = m->bytes[base.start + i];
+  for (int i = left.start; i < left.end; ++i) {
+    m->bytes[m->head] = m->bytes[left.start + i];
     ++m->head;
   }
 
-  m->bytes[m->head] = ch;
+  m->bytes[m->head] = right;
   ++m->head;
   result->end = m->head;
 
