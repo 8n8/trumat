78f156c3e0a1c66838627b19f95f33eed7edccfe
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-09 20:22:44 +0000

    Tweak to test script

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 set -e
 
 cc -c -Wall -Werror -O0 -std=c99 text.c trumat.c
-cc -Wall -Werror -O0 -std=c99 text.o trumat.o test.c
-./a.out
+cc -Wall -Werror -O0 -std=c99 text.o trumat.o test.c -o test
+./test
