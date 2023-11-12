3a46e86731e0f0e2b80bf95bf1d12b7c5a8b6420
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-12 17:14:30 +0000

    Fixed out of date valgrind script

--- valgrind.sh
+++ valgrind.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cc -c -Wall -Werror -O0 -g -std=c99 text.c trumat.c
-cc -Wall -Werror -O0 -g -std=c99 text.o trumat.o test.c
+cc -c -Wall -Werror -O0 -g -std=c99 trumat.c
+cc -Wall -Werror -O0 -g -std=c99 trumat.o test.c
 valgrind --leak-check=yes -s ./a.out
