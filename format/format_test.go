d7feb91fa32d74e3e0d988979c02325ef76f4b2b
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-15 22:31:25 +0000

    Run the formatter over the dont_change test files

--- format/format_test.go
+++ format/format_test.go
@@ -1,21 +1,52 @@
 package format
 
 import (
+	"io/fs"
+	"path/filepath"
 	"testing"
 )
 
-func TestFormat(t *testing.T) {
-	m := initMemory()
-	input := textFromString(m, `module X exposing (x)
+func isElmPath(path string) bool {
+	return path[len(path)-4:] == ".elm"
+}
 
+var I int = 0
 
-x =
-    0
-`)
+func walkDirFuncDontChange(
+	path string,
+	d fs.DirEntry,
+	err error,
+	m []byte,
+	t *testing.T) error {
 
-	formatted, _ := Format(m, input)
-
-	if input != formatted {
-		t.Errorf("expected %s but got %s", textToString(m, input), textToString(m, formatted))
+	if d.IsDir() || !isElmPath(path) {
+		return nil
 	}
+
+	m = m[:0]
+
+	contents, err := textFromFile(m, path)
+	if err != nil {
+		return err
+	}
+
+	formatted, err := Format(m, contents)
+	if err != nil {
+		return nil
+	}
+
+	if !textEqual(m, contents, formatted) {
+		t.Errorf("%s: expected %s but got %s", path, textToString(m, contents), textToString(m, formatted))
+	}
+
+	return nil
+}
+
+func TestDontChange(t *testing.T) {
+	m := initMemory()
+	_ = filepath.WalkDir(
+		"test_data/dont_change",
+		func(path string, d fs.DirEntry, err error) error {
+			return walkDirFuncDontChange(path, d, err, m, t)
+		})
 }
--- format/text.go
+++ format/text.go
@@ -1,5 +1,9 @@
 package format
 
+import (
+	"os"
+)
+
 func initMemory() []byte {
 	return make([]byte, 0, 50000000)
 }
@@ -19,4 +23,44 @@ func textFromString(m []byte, s string) Text {
 
 func textToString(m []byte, text Text) string {
 	return string(m[text.start:text.end])
+}
+
+func sliceEqual(a []byte, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func textEqual(m []byte, a Text, b Text) bool {
+	return sliceEqual(m[a.start:a.end], m[b.start:b.end])
+}
+
+func textFromFile(m []byte, path string) (Text, error) {
+	var result Text
+	file, err := os.Open(path)
+	if err != nil {
+		return result, err
+	}
+
+	result.start = uint32(len(m))
+	m = m[:cap(m)]
+
+	count, err := file.Read(m[result.start:])
+	if err != nil {
+		return result, err
+	}
+
+	m = m[:int(result.start)+count]
+
+	result.end = uint32(len(m))
+
+	return result, nil
 }
