22c56e679034468c1ce3e5e9a696fcfaa7a162e9
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-16 19:25:46 +0000

    Run the tests with the memory sanitizer

--- test.sh
+++ test.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+# Make sure that the main runner builds
+clang -c -Wall -Werror -O0 -std=c99 trumat.c
+clang -Wall -Werror -O0 -std=c99 trumat.o main.c
+
+# Run the tests with the memory sanitizer
 clang -fsanitize=memory -g -c -Wall -Werror -O0 -std=c99 trumat.c
-# Build the main runner just to check that it compiles.
-clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 trumat.o main.c
 clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 trumat.o test.c
 ./a.out
