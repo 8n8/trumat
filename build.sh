1b9e9ed254637c5d77f402a687d37ad43c4612d8
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-16 21:09:19 +0000

    Use clang instead of GCC in the build script
    The test script needs clang because it has the memory sanitizer, which
    GCC doesn't have yet.
    
    It's better to just depend on the one compiler for the whole project so that
    people don't have to install so much to get started.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cc -c -Wall -Werror -O2 -std=c99 trumat.c
-cc -Wall -O2 -Werror -std=c99 trumat.o main.c -o trumat
+clang -c -Wall -Werror -O2 -std=c99 trumat.c
+clang -Wall -O2 -Werror -std=c99 trumat.o main.c -o trumat
