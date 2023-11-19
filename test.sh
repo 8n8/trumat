f1a171a59e4627e57b8cbc2529b8d1e203a377af
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-19 10:57:51 +0000

    Redirect stdout from the second test run to /dev/null
    It prevents having two lots of test results.
    
    Previously I was hiding the results of the first test run, but the problem
    was that when the first test run is unsuccessful this means that debug
    print statements are hidden.
    
    When the first teset run fails the second run doesn't happen.

--- test.sh
+++ test.sh
@@ -9,9 +9,9 @@ clang -Wall -Werror -O0 -std=c99 trumat.o main.c
 # Run the tests with the memory sanitizer
 clang -fsanitize=memory -g -c -Wall -Werror -O0 -std=c99 trumat.c
 clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 trumat.o test.c
-./a.out >/dev/null
+./a.out
 
 # Run the tests with the address sanitizer
 clang -fsanitize=address -fno-omit-frame-pointer -g -c -Wall -Werror -O0 -std=c99 trumat.c
 clang -fsanitize=address -fno-omit-frame-pointer -g -Wall -Werror -O0 -std=c99 trumat.o test.c
-./a.out
+./a.out >/dev/null
