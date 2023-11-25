8f682c9db0f45acd52dd6de63a730813d5cb7d48
True Ghiassi <true@ghiassitrio.co.uk>
2023-11-25 18:53:24 +0000

    Documentation on the test script

--- test.sh
+++ test.sh
@@ -14,4 +14,6 @@ clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 trumat.o test.c
 # Run the tests with the address sanitizer
 clang -fsanitize=address -fno-omit-frame-pointer -g -c -Wall -Werror -O0 -std=c99 trumat.c
 clang -fsanitize=address -fno-omit-frame-pointer -g -Wall -Werror -O0 -std=c99 trumat.o test.c
+# Pipe it to /dev/null so that the test results are not printed again. The
+# results were printed by the previous run.
 ./a.out >/dev/null
