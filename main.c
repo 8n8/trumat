d797b5c098d734efbcf466c1c9bb8278a8d127bf
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-01 19:43:08 +0000

    Set up basic test runner and trivial implementation

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+const char* usage =
+  "expecting two arguments:\n"
+  "1. --overwrite to confirm it's OK to recursively overwrite all the Elm files in the path\n"
+  "2. the path to format\n"
+  "\n"
+  "for example:\n"
+  "  $ trumat --overwrite Hello.elm\n"
+  "  $ trumat --overwrite some/directory/name\n";
+
+int string_equal(char* a, char* b) {
+  for (; *a == *b && *a != '\0'; ++a, ++b) {
+  }
+  return *a == '\0' && *b == '\0';
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    fputs(usage, stderr);
+    return -1;
+  }
+
+  if (!string_equal(argv[1], "--overwrite")) {
+    fputs(usage, stderr);
+    return -1;
+  }
+
+  return 0;
+}
--- test.sh
+++ test.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-gcc test.c
-./a.out
+cp -r formatted tmp_test_formatted
+clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 main.c
+./a.out --overwrite tmp_test_formatted
+rm -rf tmp_test_formatted
+
+cp -r formatted tmp_test_formatted
+clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -O0 -std=c99 main.c
+./a.out --overwrite tmp_test_formatted
+diff formatted tmp_test_formatted
+rm -rf tmp_test_formatted
