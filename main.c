9208719c894f365b564c1e2f541d5da7c7bc0128
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-01 19:44:04 +0000

    Formatting

--- main.c
+++ main.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
+const char *usage = "expecting two arguments:\n"
+                    "1. --overwrite to confirm it's OK to recursively "
+                    "overwrite all the Elm files in the path\n"
+                    "2. the path to format\n"
+                    "\n"
+                    "for example:\n"
+                    "  $ trumat --overwrite Hello.elm\n"
+                    "  $ trumat --overwrite some/directory/name\n";
 
-const char* usage =
-  "expecting two arguments:\n"
-  "1. --overwrite to confirm it's OK to recursively overwrite all the Elm files in the path\n"
-  "2. the path to format\n"
-  "\n"
-  "for example:\n"
-  "  $ trumat --overwrite Hello.elm\n"
-  "  $ trumat --overwrite some/directory/name\n";
-
-int string_equal(char* a, char* b) {
+int string_equal(char *a, char *b) {
   for (; *a == *b && *a != '\0'; ++a, ++b) {
   }
   return *a == '\0' && *b == '\0';
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc != 3) {
     fputs(usage, stderr);
     return -1;
