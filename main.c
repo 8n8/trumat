430a8e838ed38d46a0b017e21448141efb3ab537
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-03 21:51:54 +0000

    Layed out the data shapes

--- main.c
+++ main.c
@@ -3,87 +3,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// DATA
+// ====
+//
+// Maximum supported codebase
+// --------------------------
+//
 // Some Elm stats from elm-pages:
 //
 // - 375 files
 // - 65k lines of code
-// - 3.9 MB of code
+// - 3.9MB of code
 //
-// - average 60 bytes per line of code
-// - average 10 kB per file
+// So on average there are:
 //
-// Should be enough to fit even a massive codebase. The largest Elm codebase
-// I have heard of has about 700000 lines of code.
+// - 60B per line of code
+// - 10kB per file
 //
-// Let's make this tool able to handle double that. So 1.4 million lines of
-// code. If each line of code is average 60 bytes, that's 84 MB.
-#define MAX_SRC 84 * 1000 * 1000
-uint8_t SRC[MAX_SRC]; // 84 MB
+// The biggest Elm codebase I have heard of is 700k lines of code. Let's
+// make this tool work for double that. So the stats for the largest
+// codebase this tool will support is:
+//
+// - 8250 files
+// - 1.4M lines of code
+// - 86MB of code
+//
+// There are four main types of ID:
+//
+// - source characters (u32)
+// - path characters (u32)
+// - files (u16)
+// - tokens (u32)
+// - nodes (u32)
+//
+//
+// Source characters
+// -----------------
+//
+// A source code character ID is a 32 bit integer.
+//
+//
+// Path characters
+// ---------------
+//
+// If there are 256B in a file path on average there are
+// 256B x 8250 = 2.1MB of file paths.
+//
+//
+// Files
+// -----
+//
+// A file ID is a 16 bit integer.
+//
+//
+// Tokens
+// ------
+//
+// A token ID is a 32 bit integer.
+//
+// The tokens are mostly single characters. Let's assume half as many
+// tokens as source code bytes. That's 43M tokens. And let's assume 5M
+// text tokens.
+//
+//
+// Nodes
+// -----
+//
+// A node ID is a 32 bit integer.
+//
+// There are more nodes than there are text tokens, so let's assume there
+// are 10M nodes. That means 10/1.4 = 7 nodes per line of code.
+#define MAX_SRC 86 * 1000 * 1000
+uint8_t SRC[MAX_SRC];
 
-// With average of 10kB per file that is a maximum of 8400 files.
-#define MAX_FILES 8400
-uint32_t FILE_END[MAX_FILES]; // 269 KB
-// Say there are 256 bytes per file path on average. That's 2.2 MB.
-#define MAX_PATHS 2200 * 1000
-uint8_t PATH[MAX_PATHS]; // 2.2 MB
-uint32_t PATH_END[MAX_FILES]; // 34 KB
+#define MAX_PATH 2100 * 1000
+uint8_t PATH[MAX_PATH];
+
+#define MAX_FILES 8250
+uint32_t FILE_END[MAX_FILES];
+uint16_t PATH_END[MAX_FILES];
 int NUM_FILES = 0;
 
-
-// The tokens are mostly single characters. Say every 5th token is multi
-// character. That means 5 tokens will be 9 bytes, so say 2 bytes per token
-// on average. So let's assume half as many tokens as source code bytes.
-#define MAX_TOKENS 42 * 1000 * 1000
-uint8_t TOKENS[MAX_TOKENS]; // 42 MB
+#define MAX_TOKENS 43 * 1000 * 1000
+uint8_t TOKENS[MAX_TOKENS];
 int NUM_TOKENS = 0;
 
-#define MAX_TEXT_TOKENS 8000 * 1000
-// Token IDs that have text, such as variable names or comments. The token
-// IDs are the array indices.
-uint32_t TEXT_TOKENS[MAX_TEXT_TOKENS]; // 32 MB
-uint32_t TEXT_TOKEN_START[MAX_TEXT_TOKENS]; // 32 MB
-uint16_t TEXT_TOKEN_SIZE[MAX_TEXT_TOKENS]; // 16 MB
+#define MAX_TEXT_TOKENS  5000 * 1000
+uint32_t TEXT_TOKENS[MAX_TEXT_TOKENS]; // IDs of tokens that have text
+uint32_t TEXT_TOKEN_START[MAX_TEXT_TOKENS];
+uint16_t TEXT_TOKEN_SIZE[MAX_TEXT_TOKENS];
 int NUM_TEXT_TOKENS = 0;
 
-// Let's say there are three nodes per line of code on average. That's
-// 4.2 million nodes.
-#define MAX_NODES 4200 * 1000
+#define MAX_NODES 10 * 1000 * 1000
+uint32_t PARENT[MAX_NODES];
+int NUM_NODES = 0;
+uint32_t TEXT_NODES[MAX_TEXT_TOKENS]; // IDs of nodes that have text
+uint32_t TEXT_NODE_START[MAX_TEXT_TOKENS];
+uint16_t TEXT_NODE_SIZE[MAX_TEXT_TOKENS];
+int NUM_TEXT_NODES = 0;
 
-// The largest Elm file I have ever found is 700 kB. I think it was in
-// elm-review. Let's say I need to be able to format a file that's 2 MB.
-//
-// Say there are 100 bytes per line on average. That's 20000 lines of code.
-//
-// Say there is an average of 3 nodes per line, that's 60000 nodes. That
-// fits in 16 bits.
-#define MAX_NODES 256 * 256
-uint16_t PARENT[MAX_NODES]; // 130 KB
-uint16_t NEXT_NODE = 0;
-
-// Quite a lot of the nodes will have text attached to them, such as
-// variable names or comments. Say the average text is 6 bytes and that half
-// the nodes have text. That's 200 kB.
-#define MAX_TEXT 200 * 1000
-uint8_t TEXT[MAX_TEXT]; // 200 KB
-uint32_t TEXT_END[MAX_NODES]; // 362 KB
-
-// This contains all the source code of all the Elm files. It contains all
-// the Elm modules one after another.
-uint8_t SRC[SRC_SIZE];
-
-// Using the logic for calculating the source size above, the maximum number
-// of files is 4 * 1900 = 7600.
-#define MAX_FILES 7600
-uint32_t FILE_END[MAX_FILES]; // 30 KB
-int NUM_FILES = 0;
-
-// Each file has a path. It's unlikely the average path is more than 256
-// bytes. If there are maximum 7600 files, that's 1.9 MB.
-#define MAX_PATHS 1900 * 1000
-uint8_t PATH[MAX_PATHS];
-uint16_t PATH_END[MAX_FILES];
-
-const char *usage = "expecting two arguments:\n"
+static const char *usage = "expecting two arguments:\n"
                     "1. --overwrite to confirm it's OK to recursively "
                     "overwrite all the Elm files in the path\n"
                     "2. the path to format\n"
@@ -100,34 +119,57 @@ static int string_length(char *s) {
   return n;
 }
 
-int string_equal(char *a, char *b) {
+static int string_equal(char *a, char *b) {
   for (; *a == *b && *a != '\0'; ++a, ++b) {
   }
   return *a == '\0' && *b == '\0';
 }
 
-static void read_one_src(char *path, int *src_index) {
+static void save_path(char *path) {
+  int path_index = 0;
+  if (NUM_FILES > 0) {
+    path_index = PATH_END[NUM_FILES - 1];
+  }
+
+  for (int i = 0; path[i] != '\0'; ++i, ++path_index) {
+    if (path_index >= MAX_PATH) {
+      fputs("not enough path memory\n", stderr);
+      exit(-1);
+    }
+
+    PATH[path_index] = path[i];
+  }
+
+  PATH_END[NUM_FILES] = path_index;
+}
+
+static void save_file(FILE *file) {
+  int src_index = 0;
+  if (NUM_FILES > 0) {
+    src_index = FILE_END[NUM_FILES - 1];
+  }
+
+  int n = fread(SRC + src_index, 1, MAX_SRC - src_index, file);
+  if (!feof(file)) {
+    fputs("not enough source memory\n", stderr);
+    exit(-1);
+  }
+
+  FILE_END[NUM_FILES] = src_index + n;
+}
+
+static void read_one_src(char *path) {
   FILE *file = fopen(path, "r");
   if (file == NULL) {
     return;
   }
 
-  for (int i = 0; path[i] != '\0'; ++i) {
-    SRC[*src_index] = path[i];
-    ++*src_index;
-  }
-  SRC[*src_index] = '\0';
+  save_path(path);
+  save_file(file);
 
-  int n = fread(SRC + *src_index, 1, SRC_SIZE - *src_index, file);
-  if (!feof(file)) {
-    fprintf(stderr, "Error: file %s is too big\n", path);
-    exit(-1);
-  }
+  ++NUM_FILES;
+
   fclose(file);
-
-  *src_index += n;
-  SRC[*src_index] = '\0';
-  ++*src_index;
 }
 
 static int is_elm_path(char *path) {
@@ -151,14 +193,14 @@ static void make_sub_path(char *left, char *right, char *result) {
   result[i] = '\0';
 }
 
-static void read_src(char *path, int *src_index) {
+static void read_src(char *path) {
   DIR *directory_handle = opendir(path);
   if (directory_handle == NULL && !is_elm_path(path)) {
     return;
   }
 
   if (directory_handle == NULL && is_elm_path(path)) {
-    read_one_src(path, src_index);
+    read_one_src(path);
     return;
   }
 
@@ -167,14 +209,11 @@ static void read_src(char *path, int *src_index) {
     if (entry->d_name[0] != '.') {
       char sub_path[256];
       make_sub_path(path, entry->d_name, sub_path);
-      read_src(sub_path, src_index);
+      read_src(sub_path);
     }
     entry = readdir(directory_handle);
   }
   closedir(directory_handle);
-
-  ++*src_index;
-  SRC[*src_index] = '\1';
 }
 
 int main(int argc, char *argv[]) {
@@ -183,8 +222,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  int src_index = 0;
-  read_src(argv[2], &src_index);
+  read_src(argv[2]);
 
   return 0;
 }
