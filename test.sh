5c6dd43c4f4e844d44db9ad0ccff95dac6de4b0b
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-03 22:06:25 +0000

    Make the test script work on subdirectories of the test directory

--- test.sh
+++ test.sh
@@ -12,5 +12,5 @@ rm -rf tmp_test_formatted
 cp -r formatted tmp_test_formatted
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -O0 -std=c99 main.c
 ./a.out --overwrite tmp_test_formatted > /dev/null
-diff formatted tmp_test_formatted
+diff -r formatted tmp_test_formatted
 rm -rf tmp_test_formatted
