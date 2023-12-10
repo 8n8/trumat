1e94b5124fd347837a033b13cf5f4f3688e60b36
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-10 14:55:51 +0000

    Set up tests for unformatted code

--- test.sh
+++ test.sh
@@ -2,15 +2,20 @@
 
 set -e
 
-rm -rf tmp_test_formatted
+rm -rf tmp_formatted tmp_input
 
-cp -r formatted tmp_test_formatted
+cp -r formatted tmp_formatted
+cp -r input tmp_input
 clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 main.c
-./a.out --overwrite tmp_test_formatted
-rm -rf tmp_test_formatted
+./a.out --overwrite tmp_formatted
+./a.out --overwrite tmp_input
+rm -rf tmp_formatted tmp_input
 
-cp -r formatted tmp_test_formatted
+cp -r formatted tmp_formatted
+cp -r input tmp_input
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -O0 -std=c99 main.c
-./a.out --overwrite tmp_test_formatted > /dev/null
-diff -r formatted tmp_test_formatted
-rm -rf tmp_test_formatted
+./a.out --overwrite tmp_formatted > /dev/null
+./a.out --overwrite tmp_input > /dev/null
+diff -r --color formatted tmp_formatted
+diff -r --color expected tmp_input
+rm -rf tmp_formatted tmp_input
