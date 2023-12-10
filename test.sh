4811beb6746b288ce68c5b80f0fea68158fa390d
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-10 11:42:31 +0000

    Added an unformatted test

--- test.sh
+++ test.sh
@@ -6,6 +6,13 @@ stack install --fast
 rm -rf tmp_test_formatted
 cp -r formatted tmp_test_formatted
 trumat-exe --overwrite tmp_test_formatted
-diff -r formatted tmp_test_formatted
+diff -r --color formatted tmp_test_formatted
 rm -rf tmp_test_formatted
+
+rm -rf tmp_test_input
+cp -r input tmp_test_input
+trumat-exe --overwrite tmp_test_input
+diff -r --color tmp_test_input expected
+rm -rf tmp_test_input
+
 echo "FINISHED TEST RUN"
