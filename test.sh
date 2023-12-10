423837a1b8dc3a4a688248f6f5f369de7ca06f1d
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-10 14:58:40 +0000

    Fixed order of test inputs
    It was showing the colours the wrong way round, so the incorrect value
    was green and the correct one was red.

--- test.sh
+++ test.sh
@@ -16,6 +16,6 @@ cp -r input tmp_input
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -O0 -std=c99 main.c
 ./a.out --overwrite tmp_formatted > /dev/null
 ./a.out --overwrite tmp_input > /dev/null
-diff -r --color formatted tmp_formatted
-diff -r --color expected tmp_input
+diff -r --color tmp_formatted formatted
+diff -r --color tmp_input expected
 rm -rf tmp_formatted tmp_input
