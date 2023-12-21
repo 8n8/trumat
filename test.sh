094fe0ba97029336343d24ee2f3b9f291ba8a4f3
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-21 21:05:02 +0000

    Added -Wextra and -pedantic to the compiler flags

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ rm -rf tmp_formatted tmp_input
 
 cp -r formatted tmp_formatted
 cp -r input tmp_input
-clang -fsanitize=memory -g -Wall -Werror -O0 -std=c99 main.c
+clang -fsanitize=memory -g -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
 ./a.out --overwrite tmp_formatted
 ./a.out --overwrite tmp_input
 rm -rf tmp_formatted tmp_input
@@ -14,7 +14,7 @@ rm -rf tmp_formatted tmp_input
 
 cp -r formatted tmp_formatted
 cp -r input tmp_input
-clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -O0 -std=c99 main.c
+clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
 ./a.out --overwrite tmp_formatted > /dev/null
 ./a.out --overwrite tmp_input > /dev/null
 diff -r --color tmp_formatted formatted
