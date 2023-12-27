93fc1b5ffbbbef4369164fa161e1b867845c8b7a
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-27 22:01:04 +0000

    Added elm-format as a developer dependency to speed up test creation
    The test script now runs elm-format each time it runs, so that I don't
    need to manually create the expected test results. They are generated by
    copying the inputs and running elm-format on them.

--- expected/Int/0xabcdef12.elm
+++
@@ -1,5 +0,0 @@
-module X exposing (x)
-
-
-x =
-    0xABCDEF12
--- test.sh
+++ test.sh
@@ -2,23 +2,25 @@
 
 set -e
 
-rm -rf tmp_formatted tmp_input
+rm -rf tmp_test_formatted tmp_test_input
 
-cp -r formatted tmp_formatted
-cp -r input tmp_input
+cp -r formatted tmp_test_formatted
+cp -r unformatted tmp_test_input
 clang -fsanitize=memory -g -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
-./a.out --overwrite tmp_formatted
-./a.out --overwrite tmp_input
-rm -rf tmp_formatted tmp_input
+./a.out --overwrite tmp_test_formatted
+./a.out --overwrite tmp_test_input
+rm -rf tmp_test_formatted tmp_test_input
 
 
-cp -r formatted tmp_formatted
-cp -r input tmp_input
+cp -r formatted tmp_test_formatted
+cp -r unformatted tmp_test_input
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
-./a.out --overwrite tmp_formatted > /dev/null
-./a.out --overwrite tmp_input > /dev/null
-diff -r --color tmp_formatted formatted
-diff -r --color tmp_input expected
-rm -rf tmp_formatted tmp_input
+./a.out --overwrite tmp_test_formatted > /dev/null
+./a.out --overwrite tmp_test_input > /dev/null
+diff -r --color tmp_test_formatted formatted
+cp -r unformatted tmp_test_expected
+elm-format tmp_test_expected --yes > /dev/null
+diff -r --color tmp_test_input tmp_test_expected
+rm -rf tmp_test_formatted tmp_test_input tmp_test_expected
 
-echo "`find input formatted -type f | wc -l` tests of which `find formatted -type f | wc -l` are formatted and `find input -type f | wc -l` are unformatted"
+echo "`find unformatted formatted -type f | wc -l` tests of which `find formatted -type f | wc -l` are formatted and `find unformatted -type f | wc -l` are unformatted"
