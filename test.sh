f82be5e89bcff0714dea376a24c21f2033462ac7
True Ghiassi <true@ghiassitrio.co.uk>
2023-12-28 16:27:33 +0000

    Don't delete the expected directory after running tests
    This is useful because it means I can look in the expected files when
    debugging failed tests.

--- test.sh
+++ test.sh
@@ -11,16 +11,16 @@ clang -fsanitize=memory -g -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
 ./a.out --overwrite tmp_test_input
 rm -rf tmp_test_formatted tmp_test_input
 
-
 cp -r formatted tmp_test_formatted
 cp -r unformatted tmp_test_input
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
 ./a.out --overwrite tmp_test_formatted > /dev/null
 ./a.out --overwrite tmp_test_input > /dev/null
 diff -r --color tmp_test_formatted formatted
-cp -r unformatted tmp_test_expected
-elm-format tmp_test_expected --yes > /dev/null
-diff -r --color tmp_test_input tmp_test_expected
-rm -rf tmp_test_formatted tmp_test_input tmp_test_expected
+rm -rf expected
+cp -r unformatted expected
+elm-format expected --yes > /dev/null
+diff -r --color tmp_test_input expected
+rm -rf tmp_test_formatted tmp_test_input
 
 echo "`find unformatted formatted -type f | wc -l` tests of which `find formatted -type f | wc -l` are formatted and `find unformatted -type f | wc -l` are unformatted"
