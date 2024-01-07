3c98f0cfc559a8c73b11efc7b421f143153456ef
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-07 18:55:20 +0000

    Two empty block comments before module exposing keyword

--- main.c
+++ main.c
@@ -348,7 +348,7 @@ static void many_whitespace_parse() {
 }
 
 static int is_after_keyword_char(uint8_t c) {
-  return c == ' ' || c == '\n' || c == '{' || c == '-';
+  return c == ' ' || c == '\n' || c == '{' || c == '-' || c == '(';
 }
 
 static int after_keyword_parse() {
