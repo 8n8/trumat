daee0f8b0b96c47c173152c1a5a60d5fcd1541f1
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-13 19:03:38 +0000

    Don't create test input directory till after successful compilation

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@
 set -e
 
 rm -rf got
-cp -r input got
 clang -fsanitize=memory -g -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
+cp -r input got
 ./a.out --overwrite got
 
 rm -rf got
-cp -r input got
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
+cp -r input got
 ./a.out --overwrite got > /dev/null
 rm -rf expected
 cp -r input expected
