46774f8e35fc0037a480959309112bca199627c9
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-20 15:07:51 +0000

    Simple integer literals with 3 or more characters

--- src/IntSimpleMulti.hs
+++ src/IntSimpleMulti.hs
@@ -1,6 +1,7 @@
 module IntSimpleMulti (IntSimpleMulti, parse, write) where
 
 import Data.Attoparsec.ByteString.Char8 (Parser)
+import qualified Data.Attoparsec.ByteString.Char8
 import Data.ByteString (ByteString)
 import Digit (Digit)
 import qualified Digit
@@ -8,15 +9,16 @@ import NonZeroDigit (NonZeroDigit)
 import qualified NonZeroDigit
 
 data IntSimpleMulti
-  = IntSimpleMulti NonZeroDigit Digit
+  = IntSimpleMulti NonZeroDigit Digit [Digit]
 
 parse :: Parser IntSimpleMulti
 parse =
   do
     first <- NonZeroDigit.parse
     second <- Digit.parse
-    pure (IntSimpleMulti first second)
+    subsequent <- Data.Attoparsec.ByteString.Char8.many' Digit.parse
+    pure (IntSimpleMulti first second subsequent)
 
 write :: IntSimpleMulti -> ByteString
-write (IntSimpleMulti first second) =
-  NonZeroDigit.write first <> Digit.write second
+write (IntSimpleMulti first second subsequent) =
+  NonZeroDigit.write first <> Digit.write second <> mconcat (map Digit.write subsequent)
