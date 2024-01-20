bebacb1e8b74570964c058a81b02fd9aab882edf
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-20 21:22:49 +0000

    Hex literal 0x0123456789ABCDEF

--- src/HexDigit.hs
+++ src/HexDigit.hs
@@ -6,12 +6,57 @@ import Data.ByteString (ByteString)
 
 data HexDigit
   = H0
+  | H1
+  | H2
+  | H3
+  | H4
+  | H5
+  | H6
+  | H7
+  | H8
+  | H9
+  | A
+  | B
+  | C
+  | D
+  | E
+  | F
 
 write :: HexDigit -> ByteString
 write digit =
   case digit of
     H0 ->
       "0"
+    H1 ->
+      "1"
+    H2 ->
+      "2"
+    H3 ->
+      "3"
+    H4 ->
+      "4"
+    H5 ->
+      "5"
+    H6 ->
+      "6"
+    H7 ->
+      "7"
+    H8 ->
+      "8"
+    H9 ->
+      "9"
+    A ->
+      "A"
+    B ->
+      "B"
+    C ->
+      "C"
+    D ->
+      "D"
+    E ->
+      "E"
+    F ->
+      "F"
 
 parse :: Parser HexDigit
 parse =
@@ -28,5 +73,35 @@ charToHexDigit char =
   case char of
     '0' ->
       Just H0
+    '1' ->
+      Just H1
+    '2' ->
+      Just H2
+    '3' ->
+      Just H3
+    '4' ->
+      Just H4
+    '5' ->
+      Just H5
+    '6' ->
+      Just H6
+    '7' ->
+      Just H7
+    '8' ->
+      Just H8
+    '9' ->
+      Just H9
+    'A' ->
+      Just A
+    'B' ->
+      Just B
+    'C' ->
+      Just C
+    'D' ->
+      Just D
+    'E' ->
+      Just E
+    'F' ->
+      Just F
     _ ->
       Nothing
