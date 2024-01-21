0253118617092a0c561bde89e22d3b9908fbda09
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-21 19:24:34 +0000

    Float literal witn an exponent

--- src/Float_.hs
+++ src/Float_.hs
@@ -4,33 +4,26 @@ import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8
 import Data.ByteString (ByteString)
 import Data.Function ((&))
-import Digit (Digit)
-import qualified Digit
+import FloatExponent (FloatExponent)
+import qualified FloatExponent
+import FloatSimple (FloatSimple)
+import qualified FloatSimple
 
 data Float_
-  = Float_ Digit [Digit] Digit [Digit]
+  = Simple FloatSimple
+  | Exponent FloatExponent
 
 write :: Float_ -> ByteString
-write (Float_ before1 before2s after1 after2s) =
-  [ Digit.write before1,
-    mconcat (map Digit.write before2s),
-    ".",
-    Digit.write after1,
-    mconcat (map Digit.write after2s)
-  ]
-    & mconcat
-
-someDigits :: Parser (Digit, [Digit])
-someDigits =
-  do
-    first <- Digit.parse
-    subsequent <- Data.Attoparsec.ByteString.Char8.many' Digit.parse
-    pure (first, subsequent)
+write float =
+  case float of
+    Simple f ->
+      FloatSimple.write f
+    Exponent f ->
+      FloatExponent.write f
 
 parse :: Parser Float_
 parse =
-  do
-    (before1, before2s) <- someDigits
-    _ <- Data.Attoparsec.ByteString.Char8.char '.'
-    (after1, after2s) <- someDigits
-    pure (Float_ before1 before2s after1 after2s)
+  [ fmap Exponent FloatExponent.parse,
+    fmap Simple FloatSimple.parse
+  ]
+    & Data.Attoparsec.ByteString.Char8.choice
