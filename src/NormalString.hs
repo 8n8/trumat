07e75ef4e7b1392f794f8ea4909458089dd82c37
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-22 19:55:07 +0000

    String literal "a"

--- src/NormalString.hs
+++ src/NormalString.hs
@@ -3,16 +3,25 @@ module NormalString (NormalString, write, parse) where
 import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8
 import Data.ByteString (ByteString)
+import Data.Function ((&))
+import NormalStringItem (NormalStringItem)
+import qualified NormalStringItem
 
 data NormalString
-  = Empty
+  = NormalString [NormalStringItem]
 
 parse :: Parser NormalString
 parse =
   do
-    _ <- Data.Attoparsec.ByteString.Char8.string "\"\""
-    pure Empty
+    _ <- Data.Attoparsec.ByteString.Char8.char '"'
+    items <- Data.Attoparsec.ByteString.Char8.many' NormalStringItem.parse
+    _ <- Data.Attoparsec.ByteString.Char8.char '"'
+    pure (NormalString items)
 
 write :: NormalString -> ByteString
-write _ =
-  "\"\""
+write (NormalString items) =
+  [ "\"",
+    mconcat (map NormalStringItem.write items),
+    "\""
+  ]
+    & mconcat
