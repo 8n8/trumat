7cda5295fa1cfea2cbdc63ee27b6f38946e400fa
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-23 20:49:55 +0000

    Removed unused code

--- main.c
+++ main.c
@@ -21,23 +21,6 @@ static FILE *OUT;
 // - max 5000 lines of code in a single top-level definition
 // - average 60 bytes per line of code
 // - average 5 nodes per line
-#define MAX_NODE 25000 // 5000 * 5
-static uint16_t CHILD[MAX_NODE];
-static uint16_t SIBLING[MAX_NODE];
-static uint8_t NODE_TYPE[MAX_NODE];
-static uint32_t SRC_START[MAX_NODE];
-static uint16_t SRC_SIZE[MAX_NODE];
-// 0 is the null node
-// 1 is the root
-#define ROOT 1
-// So the first non-root node is 2
-static int num_node = 2;
-
-static uint16_t module_exports_all_node = 0;
-
-#define max_hanging_block_comment_node 200
-static uint16_t hanging_block_comment_node[max_hanging_block_comment_node];
-static int num_hanging_block_comment_node = 0;
 
 void dbg_src() {
   for (int i = 0; i < NUM_SRC; ++i) {
@@ -58,115 +41,6 @@ enum node_type {
   COMPACT_BLOCK_COMMENT_NODE,
 };
 
-static int is_hanging_block_comment_node(uint16_t id) {
-  for (int i = 0; i < num_hanging_block_comment_node; ++i) {
-    if (hanging_block_comment_node[i] == id) {
-      return 1;
-    }
-  }
-  return 0;
-}
-
-static int is_no_docs_node(uint16_t id) {
-  return NODE_TYPE[id] == NO_DOCS_NODE;
-}
-
-static int is_module_expose_all_variants_node(uint16_t id) {
-  return NODE_TYPE[id] == MODULE_EXPOSE_ALL_VARIANTS_NODE;
-}
-
-static int is_single_line_block_comment_node(uint16_t id) {
-  return NODE_TYPE[id] == SINGLE_LINE_BLOCK_COMMENT_NODE;
-}
-
-static int is_compact_block_comment_node(uint16_t id) {
-  return NODE_TYPE[id] == COMPACT_BLOCK_COMMENT_NODE;
-}
-
-static int is_empty_block_comment_node(uint16_t id) {
-  return NODE_TYPE[id] == EMPTY_BLOCK_COMMENT_NODE;
-}
-
-static int is_text_node(uint16_t id) {
-  return is_module_expose_all_variants_node(id) ||
-         is_single_line_block_comment_node(id);
-}
-
-static char *node_type_to_string(uint16_t id) {
-  if (is_no_docs_node(id)) {
-    return "NODO";
-  }
-  if (is_module_expose_all_variants_node(id)) {
-    return "MEXA";
-  }
-  if (is_empty_block_comment_node(id)) {
-    return "EBLK";
-  }
-  if (is_compact_block_comment_node(id)) {
-    return "MLCB";
-  }
-  if (is_hanging_block_comment_node(id)) {
-    return "HBCB";
-  }
-  if (is_single_line_block_comment_node(id)) {
-    return "SBLK";
-  }
-  return "EXAL";
-}
-
-void dbg_siblings() {
-  puts("siblings:");
-  for (int i = 0; i < num_node; ++i) {
-    printf("%04d ", SIBLING[i]);
-  }
-  putchar('\n');
-}
-
-void dbg_children() {
-  puts("children:");
-
-  for (int i = 0; i < num_node; ++i) {
-    printf("%04d ", CHILD[i]);
-  }
-  putchar('\n');
-
-  for (int i = 0; i < num_node; ++i) {
-    printf("%s ", node_type_to_string(i));
-  }
-  putchar('\n');
-}
-
-void dbg_literals() {
-  puts("literals:");
-  for (int i = 0; i < num_node; ++i) {
-    const uint16_t start = SRC_START[i];
-    const uint16_t size = SRC_SIZE[i];
-    if (!is_text_node(i)) {
-      continue;
-    }
-    printf("%04d ", i);
-    for (int j = start; j < start + size; ++j) {
-      if (SRC[j] == '\n') {
-        fputs("\\n", stdout);
-        continue;
-      }
-      fputc(SRC[j], stdout);
-    }
-    putchar('\n');
-  }
-  putchar('\n');
-}
-
-void dbg_ast() {
-  for (int i = 0; i < num_node; ++i) {
-    printf("%04d ", i);
-  }
-  putchar('\n');
-  dbg_children();
-  dbg_siblings();
-  dbg_literals();
-}
-
 static const char *usage = "expecting two arguments:\n"
                            "1. --overwrite to confirm it's OK to recursively "
                            "overwrite all the Elm files in the path\n"
@@ -182,17 +56,9 @@ static int string_equal(char *a, char *b) {
   return *a == '\0' && *b == '\0';
 }
 
-static void zero_ast() {
-  num_node = 2;
-  module_exports_all_node = 0;
-  num_hanging_block_comment_node = 0;
-}
-
 static int with_out_file() {
   I = -1;
-  zero_ast();
   fputs("module X exposing (x)\n\n\nx =\n    0\n", OUT);
-
   return 0;
 }
 
