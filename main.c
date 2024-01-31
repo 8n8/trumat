8ed581fe4aaff8cfc1f355adec82bde0fdaaba01
True Ghiassi <true@ghiassitrio.co.uk>
2024-01-31 19:03:47 +0000

    Small refactoring of comment parser

--- main.c
+++ main.c
@@ -523,11 +523,6 @@ static int line_comment_parse(int *node) {
   const int end = I;
   while (char_parse(' ') == 0) {
   }
-  if (char_parse('\n')) {
-    I = start;
-    return -1;
-  }
-
   *node = get_new_node();
   append_has_src(*node, start + 1, end - start);
   return 0;
@@ -538,13 +533,8 @@ static int block_comment_parse(int *node) {
   if (chunk_parse("{--}")) {
     return -1;
   }
-  const int end = I;
-  if (char_parse('\n')) {
-    I = start;
-    return -1;
-  }
   *node = get_new_node();
-  append_has_src(*node, start + 1, end - start);
+  append_has_src(*node, start + 1, I - start);
   return 0;
 }
 
@@ -561,6 +551,7 @@ static int module_parse(int *node) {
   }
   int left_comment;
   const int left_comment_result = comment_parse(&left_comment);
+  char_parse('\n');
   while (char_parse(' ') == 0) {
   }
   if (expression_parse(node)) {
