2c9ea8250639229609cc40309f2176f0f5d5b3a8
True Ghiassi <true@ghiassitrio.co.uk>
2024-02-12 21:52:32 +0000

    List containing function call with multiline block before the first argument

--- main.c
+++ main.c
@@ -512,14 +512,14 @@ static void function_call_write(int node, int indent) {
   int argument;
   int start = 0;
   get_argument(node, &argument, &start);
-  left_comments_write(argument, indent + 4);
+  left_comments_write(argument, floor_to_four(indent + 4));
   const int left_is_multiline = has_multiline_left_comment(argument);
   const int has_left = has_left_comment(argument);
   if (has_left && !left_is_multiline) {
     fputc(' ', OUT);
   }
   if (has_left && left_is_multiline) {
-    indent_write(indent + 4);
+    indent_write(floor_to_four(indent + 4));
   }
   expression_write(argument, indent + 4);
   while (get_argument(node, &argument, &start) == 0) {
