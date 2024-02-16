2a6fa2a3e4c94b65640f9b5c6b3c2ea4649e8159
True Ghiassi <true@ghiassitrio.co.uk>
2024-02-16 21:42:13 +0000

    Test for gappy line comments after plus sign

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm a.out
-cp input/PlusInfix/TwoLineCommentsAfterPlus.elm test.elm
+cp input/PlusInfix/GappyLineCommentsAfterPlus.elm test.elm
 clang -g -O0 -fsanitize=memory main.c
 gdb --args a.out --overwrite test.elm
