acca1f79e6f0d958b251175e31b3dc71e3c2bb81
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-04 22:33:22 +0000

    Two multiline block comments after then in if then else statement

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm a.out
-cp input/Parens/InArgument.elm test.elm
+cp input/IfThenElse/TwoMultilineBlocksAfterThen.elm test.elm
 clang -g -O0 -fsanitize=memory main.c
 gdb --args a.out --overwrite test.elm
