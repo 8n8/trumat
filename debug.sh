7f677fe029ec7037f26b60b1b25c10ea8e301a31
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-09 20:06:27 +0000

    Multiline block comment after case in case of expression

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm a.out
-cp input/IfThenElse/TwoSingleLineBlocksAfterElse.elm test.elm
+cp input/CaseOf/MultilineBlockAfterCase.elm test.elm
 clang -g -O0 -fsanitize=memory main.c
 gdb --args a.out --overwrite test.elm
