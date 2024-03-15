c5a5d69d8b2f00915769c7ac46f55c91ceb70990
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-15 19:57:23 +0000

    Upper name in parentheses in argument in pattern in case of expression

--- main.c
+++ main.c
@@ -4121,7 +4121,7 @@ static int pattern_argument_in_unnecessary_parens_contents_parse(int *node) {
   return pattern_argument_in_unnecessary_parens_parse(node) &&
          non_empty_tuple_pattern_parse(node) && empty_tuple_parse(node) &&
          non_empty_list_pattern_parse(node) && empty_list_parse(node) &&
-         lower_name_parse(node);
+         upper_name_parse(node) && lower_name_parse(node);
 }
 
 static int pattern_argument_in_unnecessary_parens_parse(int *node) {
