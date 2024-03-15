6807d3c30511c9b1dc4a12a17114d5ab77a59c13
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-15 22:13:28 +0000

    Empty tuple in infix in pattern in case of expression

--- main.c
+++ main.c
@@ -4365,7 +4365,7 @@ static int simple_expression_parse(int *node) {
 static int infixed_pattern_item_parse(int *node) {
   return non_empty_tuple_pattern_parse(node) && wildcard_parse(node) &&
          lower_name_parse(node) && empty_list_parse(node) &&
-         non_empty_list_pattern_parse(node);
+         non_empty_list_pattern_parse(node) && empty_tuple_parse(node);
 }
 
 static int infixed_item_parse(int *node) {
