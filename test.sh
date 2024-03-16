522c25232d1c91cef20389f5094595765c390a06
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-16 20:25:20 +0000

    Revert "Renamed the test directory"
    This reverts commit 23b0f13ddb5579a38fbee50473e8a06ed33ae096.

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@ set -e
 
 rm -rf got
 clang -fsanitize=memory -g -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
-cp -r tests got
+cp -r input got
 ./a.out --overwrite got
 
 rm -rf got
 clang -fsanitize=address -g -fno-omit-frame-pointer -Wall -Werror -Wextra -pedantic -O0 -std=c99 main.c
-cp -r tests got
+cp -r input got
 ./a.out --overwrite got > /dev/null
 rm -rf expected
-cp -r tests expected
+cp -r input expected
 elm-format expected --yes > /dev/null
 diff -r --color got expected
 
-echo "`find tests -type f | wc -l` tests"
+echo "`find input -type f | wc -l` tests"
