42d43539abb92ae5790c43b999f0dfb556f219b9
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-23 21:48:32 +0000

    Line comment before colon in list

---
+++ expected/Type/LineCommentBeforeColonInList.elm
@@ -0,0 +1,14 @@
+module X exposing (x)
+
+
+x =
+    [ let
+        a
+        --
+            :
+            Int
+        a =
+            0
+      in
+      a
+    ]
---
+++ input/Type/LineCommentBeforeColonInList.elm
@@ -0,0 +1,8 @@
+module X exposing (x)
+
+
+x =
+    [let
+        a--
+         : Int
+        a = 0 in a]
