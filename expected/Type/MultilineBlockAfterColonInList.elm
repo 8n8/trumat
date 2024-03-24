7cfd6ba78cf571b954946d6dd63b04af478f4955
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-24 16:39:51 +0000

    Multiline block comment after colon in type signature in list

---
+++ expected/Type/MultilineBlockAfterColonInList.elm
@@ -0,0 +1,15 @@
+module X exposing (x)
+
+
+x =
+    [ let
+        a :
+            {- b
+               c
+            -}
+            Int
+        a =
+            0
+      in
+      a
+    ]
---
+++ input/Type/MultilineBlockAfterColonInList.elm
@@ -0,0 +1,9 @@
+module X exposing (x)
+
+
+x =
+    [let
+        a : {-b
+        c-} Int
+        a = 0
+      in a]
