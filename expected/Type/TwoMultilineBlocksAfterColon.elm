21d5cdde4b24767d2e24e59c67df0552e3854bb8
True Ghiassi <true@ghiassitrio.co.uk>
2024-03-24 16:45:23 +0000

    Two multiline block comments after colon in type signature

---
+++ expected/Type/TwoMultilineBlocksAfterColon.elm
@@ -0,0 +1,17 @@
+module X exposing (x)
+
+
+x =
+    let
+        a :
+            {- a
+               b
+            -}
+            {- c
+               d
+            -}
+            Int
+        a =
+            0
+    in
+    a
---
+++ input/Type/TwoMultilineBlocksAfterColon.elm
@@ -0,0 +1,10 @@
+module X exposing (x)
+
+
+x =
+    let
+        a : {-a
+        b-}
+        {-c
+        d-} Int
+        a = 0 in a
